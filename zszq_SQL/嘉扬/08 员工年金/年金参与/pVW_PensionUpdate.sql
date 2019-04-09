6f8c2725ed33cbbf9f7b34dd99444421b7fdd744
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-04-09 14:59:31 +0800

    更新企业年金参与查看视图

--- zszq_SQL/嘉扬/08 员工年金/年金参与/pVW_PensionUpdate.sql
+++ zszq_SQL/嘉扬/08 员工年金/年金参与/pVW_PensionUpdate.sql
@@ -1,14 +1,15 @@
 -- pVW_PensionUpdate
+
 -- 非投理顾员工
-select a.IsPension as IsPension,b.Name as Name,c.Certno as Identification,b.CompID as CompID,dbo.eFN_getdepid1st(b.DepID) as DepID1,dbo.eFN_getdepid2nd(b.DepID) as DepID2,
-dbo.eFN_getdepid3th(b.DepID) as DepID3,d.DepType as DepType,b.JobID as JobID,b.Status as Status,e.JoinDate as JoinDate,e.LeaDate as LeaDate,a.SalaryPayID as SalaryPayID,
-f.Xorder as Xorder
-from pEmployeeEmolu a,eEmployee b,eDetails c,oDepartment d,eStatus e,oJob f
-where a.EID=b.EID and a.EID=c.EID and b.DepID=d.DepID and a.EID=e.EID and b.JobID=f.JobID
+select a.IsPension as IsPension,b.Name as Name,d.Certno as Identification,b.CompID as CompID,b.DepID1st as DepID1,b.DepID2nd as DepID2,
+b.DepType as DepType,b.JobTitle as JobTitle,b.Status as Status,b.JoinDate as JoinDate,b.LeaDate as LeaDate,c.SalaryPayID as SalaryPayID,
+b.JobXorder as Xorder
+from pPensionUpdatePerEmp a,pVW_Employee b,pEmployeeEmolu c,eDetails d
+where a.EID=b.EID and a.EID=C.EID and a.EID=d.EID
 
 -- 投理顾员工
 UNION
-select a.IsPension as IsPension,a.Name as Name,a.Identification as Identification,11 as CompID,a.SupDepID as DepID1,a.DepID as DepID2,NULL as DepID3,b.DepType as DepTyep
-,a.JobID as JobID,a.Status as Status,a.JoinDate as JoinDate,a.LeaveDate as LeaDate,a.SalaryPayID as SalaryPayID,b.Xorder*10000+4030 as Xorder
-from pSalesDepartMarketerEmolu a,oDepartment b
-where ISNULL(a.DepID,a.SupDepID)=b.DepID
+select a.IsPension as IsPension,a.Name as Name,a.Identification as Identification,11 as CompID,a.SupDepID as DepID1,a.DepID as DepID2,b.DepType as DepTyep,
+(select JobAbbr from oJob where JobID=a.JobID) as JobTitle,a.Status as Status,a.JoinDate as JoinDate,c.LeaveDate as LeaDate,c.SalaryPayID as SalaryPayID,b.Xorder*10000+4030 as Xorder
+from pPensionUpdatePerSDM a,oDepartment b,pSalesDepartMarketerEmolu c
+where ISNULL(a.DepID,a.SupDepID)=b.DepID and a.Identification=c.Identification

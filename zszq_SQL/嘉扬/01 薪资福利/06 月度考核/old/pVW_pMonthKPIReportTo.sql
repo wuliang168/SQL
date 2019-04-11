e403dfa5711262de651a5ee78cf374b7b8e44d25
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-04-11 16:15:08 +0800

    新增pStatus和EID

--- zszq_SQL/嘉扬/01 薪资福利/06 月度考核/old/pVW_pMonthKPIReportTo.sql
+++ zszq_SQL/嘉扬/01 薪资福利/06 月度考核/old/pVW_pMonthKPIReportTo.sql
@@ -1,7 +1,17 @@
 -- pVW_pMonthKPIReportTo
 
-select b.Badge as Badge,B.Name as Name,b.CompID as CompID,
-dbo.eFN_getdepid1st(b.DepID) as DepID1st,dbo.eFN_getdepid2nd(b.DepID) as DepID2nd,a.KPIDepID,
-a.jobid,ISNULL(c.Director,c.Director2) as KPIReportTo
+select a.EID as EID,b.Badge as Badge,B.Name as Name,b.CompID as CompID,
+dbo.eFN_getdepid1st(b.DepID) as DepID1st,dbo.eFN_getdepid2nd(b.DepID) as DepID2nd,a.KPIDepID as KPIDepID,
+a.jobid as JobID,ISNULL(c.Director,c.Director2) as KPIReportTo,a.pStatus as pStatus
 from pEmployee_Register a,pVW_Employee b,oDepartment c
-where a.EID=b.EID and a.kpidepid=c.DepID
+where a.EID=b.EID and a.kpidepid=c.DepID and c.DepID not in (359)
+
+-- 风险管理部(359)
+UNION
+select a.EID as EID,b.Badge as Badge,B.Name as Name,b.CompID as CompID,
+dbo.eFN_getdepid1st(b.DepID) as DepID1st,dbo.eFN_getdepid2nd(b.DepID) as DepID2nd,a.KPIDepID as KPIDepID,a.jobid as JobID,
+(case when dbo.eFN_getdepid2nd(b.DepID)=b.DepID and a.EID<>dbo.eFN_getdepdirector(b.DepID) then dbo.eFN_getdepdirector(b.DepID) 
+when dbo.eFN_getdepid2nd(b.DepID)=b.DepID and a.EID=dbo.eFN_getdepdirector(b.DepID) then dbo.eFN_getdepdirector(dbo.eFN_getdepid1st(b.DepID))  
+else dbo.eFN_getdepdirector2(dbo.eFN_getdepid1st(b.DepID)) end) as KPIReportTo,a.pStatus as pStatus
+from pEmployee_Register a,pVW_Employee b
+where a.EID=b.EID and a.kpidepid=359

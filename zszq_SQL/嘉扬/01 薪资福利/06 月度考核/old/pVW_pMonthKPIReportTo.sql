65bd8eb9b902df7a6a615dc6c671dddd465f74ec
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-05-22 08:44:38 +0800

    更新：调整考核人信息

--- zszq_SQL/嘉扬/01 薪资福利/06 月度考核/old/pVW_pMonthKPIReportTo.sql
+++ zszq_SQL/嘉扬/01 薪资福利/06 月度考核/old/pVW_pMonthKPIReportTo.sql
@@ -1,10 +1,28 @@
 -- pVW_pMonthKPIReportTo
 
+---- 考核部门与本部门相同(主要针对合规风控)
 select a.EID as EID,b.Badge as Badge,B.Name as Name,b.CompID as CompID,
 dbo.eFN_getdepid1st(b.DepID) as DepID1st,dbo.eFN_getdepid2nd(b.DepID) as DepID2nd,a.KPIDepID as KPIDepID,a.jobid as JobID,
-(case when dbo.eFN_getdepid2nd(b.DepID)=b.DepID and a.EID<>dbo.eFN_getdepdirector(b.DepID) then dbo.eFN_getdepdirector(b.DepID) 
-when dbo.eFN_getdepid2nd(b.DepID)=b.DepID and a.EID=dbo.eFN_getdepdirector(b.DepID) then dbo.eFN_getdepdirector(dbo.eFN_getdepid1st(b.DepID))
-when dbo.eFN_getdepid1st(b.DepID)=b.DepID and a.EID<>dbo.eFN_getdepdirector(b.DepID) then dbo.eFN_getdepdirector(dbo.eFN_getdepid1st(b.DepID))
-else dbo.eFN_getdepdirector2(dbo.eFN_getdepid1st(b.DepID)) end) as KPIReportTo,a.pStatus as pStatus
+(case 
+-- 二级部门，且非二级部门负责人
+when dbo.eFN_getdepid2nd(b.DepID)=b.DepID and a.EID<>ISNULL(dbo.eFN_getdepdirector(b.DepID),0) 
+then ISNULL(ISNULL(dbo.eFN_getdepdirector(b.DepID),dbo.eFN_getdepdirector(dbo.eFN_getdepid1st(b.DepID))),dbo.eFN_getdepdirector2(dbo.eFN_getdepid1st(b.DepID)))
+-- 二级部门，且是二级部门负责人
+when dbo.eFN_getdepid2nd(b.DepID)=b.DepID and a.EID=ISNULL(dbo.eFN_getdepdirector(b.DepID),0) 
+then ISNULL(dbo.eFN_getdepdirector(dbo.eFN_getdepid1st(b.DepID)),dbo.eFN_getdepdirector2(dbo.eFN_getdepid1st(b.DepID)))
+-- 一级部门，且非一级部门负责人
+when dbo.eFN_getdepid1st(b.DepID)=b.DepID and a.EID<>ISNULL(dbo.eFN_getdepdirector(b.DepID),0) 
+then ISNULL(dbo.eFN_getdepdirector(b.DepID),dbo.eFN_getdepdirector2(b.DepID))
+-- 一级部门，且是一级部门负责人
+else dbo.eFN_getdepdirector2(dbo.eFN_getdepid1st(b.DepID)) 
+end) as KPIReportTo,a.pStatus as pStatus
 from pEmployee_Register a,pVW_Employee b
-where a.EID=b.EID
+where a.EID=b.EID and a.KPIDepID<>737
+
+---- 考核部门与本部门不相同(主要针对合规风控)
+UNION
+select a.EID as EID,b.Badge as Badge,B.Name as Name,b.CompID as CompID,
+dbo.eFN_getdepid1st(b.DepID) as DepID1st,dbo.eFN_getdepid2nd(b.DepID) as DepID2nd,a.KPIDepID as KPIDepID,a.jobid as JobID,
+dbo.eFN_getdepdirector(a.DepID) as KPIReportTo,a.pStatus as pStatus
+from pEmployee_Register a,pVW_Employee b
+where a.EID=b.EID and a.KPIDepID=737

8a4c0a835d243777ba33d171aa59739130e6d080
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-05-30 14:31:12 +0800

    Update:更新添加HR关闭状态判断功能

--- zszq_SQL/嘉扬/00 公共组件/skyportaltask_添加考核all.sql
+++ zszq_SQL/嘉扬/00 公共组件/skyportaltask_添加考核all.sql
@@ -219,7 +219,7 @@ N'<a href="#" onclick="$x.top().LoadPortal(''1.0.570410'',''目标责任考核''
 a.ReportTo AS approver, 1 AS id
 FROM pTrgtRspCntrDep a,pTrgtRspCntr_Process b
 WHERE ISNULL(b.Submit,0)=1 and ISNULL(b.Closed,0)=0 
-and ISNULL(a.IsSubmit,0)=0 and ISNULL(a.SubmitTime,0)=0 and a.TRCLev=1
+and ISNULL(a.IsSubmit,0)=0 and ISNULL(a.SubmitTime,0)=0 and ISNULL(a.IsClosed,0)=0 and a.TRCLev=1
 and DATEDIFF(dd,GETDATE(),'2019-5-30 0:0:0')>0
 ---- 部门负责人考核
 UNION

b8437aed835f52b7b2d54e92de05b41709aa3b2c
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-06-11 17:15:12 +0800

    Update:调整优化业绩考核递交程序

--- zszq_SQL/嘉扬/02 人员发展/24 业绩考核/eSP_pEMPTrgtRspCntrMMSelfSubmit.sql
+++ zszq_SQL/嘉扬/02 人员发展/24 业绩考核/eSP_pEMPTrgtRspCntrMMSelfSubmit.sql
@@ -4,14 +4,14 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-ALTER  Procedure [dbo].[eSP_pEMPTrgtRspCntrMMSubmit]
--- skydatarefresh eSP_pEMPTrgtRspCntrMMSubmit
+ALTER Procedure [dbo].[eSP_pEMPTrgtRspCntrMMSelfSubmit]
+-- skydatarefresh eSP_pEMPTrgtRspCntrMMSelfSubmit
     @EID int,
     @RetVal int=0 Output
 As
 /*
 -- Create By wuliang E004205
--- 业务考核递交程序
+-- 业务考核员工考核递交程序
 -- @EID 为业务考核递交员工对应EID
 -- @Status为业务考核递交阶段
 */
@@ -44,26 +44,6 @@ Begin
         Return @RetVal
     End
 
-    -- 业务考核评语为空，无法递交！
-    ---- 主管部门负责人考核意见
-    IF Exists(select 1 from pEMPTrgtRspCntrKPIMM a,pEMPTrgtRspCntrMM b,pVW_TrgtRspCntrReportTo c
-    where a.KPIID=b.KPIID and b.SubmitSelf=1 and b.SubmitRT is NULL
-    and c.ReportTo=@EID and a.EID=c.EID
-    and a.CommRT is NULL)
-    Begin
-        Set @RetVal=930550
-        Return @RetVal
-    End
-    ---- 主管部门负责人反馈意见
-    IF Exists(select 1 from pEMPTrgtRspCntrKPIMM a,pEMPTrgtRspCntrMM b,pVW_TrgtRspCntrReportTo c
-    where a.KPIID=b.KPIID and ISNULL(b.SubmitHR,0)=1 and b.SubmitRRT is NULL
-    and c.ReportTo=@EID and a.EID=c.EID
-    and a.CommRRT is NULL)
-    Begin
-        Set @RetVal=930550
-        Return @RetVal
-    End
-
 
     Begin TRANSACTION
 
@@ -84,38 +64,7 @@ Begin
     -- 异常流程
     If @@Error<>0
     Goto ErrM
-    ---- 部门负责人业务考核递交
-    update a
-    set a.SubmitRT=1,a.DateRT=GETDATE()
-    from pEMPTrgtRspCntrMM a,pVW_TrgtRspCntrReportTo b
-    where b.ReportTo=@EID and a.EID=b.EID and ISNULL(a.SubmitRT,0)=0
-    -- 异常流程
-    If @@Error<>0
-    Goto ErrM
-    ---- 部门业务考核递交
-    update a
-    set a.IsSubmit=1,a.SubmitTime=GETDATE()
-    from pTrgtRspCntrDep a
-    where a.ReportTo=@EID and a.TRCLev=2
-    -- 异常流程
-    If @@Error<>0
-    Goto ErrM
-    ---- 部门负责人业务考核反馈递交
-    update a
-    set a.SubmitRRT=1,a.DateRRT=GETDATE()
-    from pEMPTrgtRspCntrMM a,pVW_TrgtRspCntrReportTo b
-    where b.ReportTo=@EID and a.EID=b.EID and ISNULL(a.SubmitRRT,0)=0 and ISNULL(a.SubmitHR,0)=1
-    -- 异常流程
-    If @@Error<>0
-    Goto ErrM
-    ---- 部门业务考核递交
-    update a
-    set a.IsSubmit=1,a.SubmitTime=GETDATE()
-    from pTrgtRspCntrDep a
-    where a.ReportTo=@EID and a.TRCLev=3
-    -- 异常流程
-    If @@Error<>0
-    Goto ErrM
+
 
     -- 更新员工目标任务协议表项pEMPTrgtRspCntr_KPI
     update a
@@ -123,7 +72,6 @@ Begin
     a.TRCAchRate=b.TRCAchRate,a.TRCAchDate=b.TRCMonth
     from pEMPTrgtRspCntr_KPI a,pEMPTrgtRspCntrKPIMM b
     where a.KPIID=b.KPIID and a.TRCKPI=b.TRCKPI and b.EID=@EID and a.EID=b.EID 
-    and ISNULL(b.SubmitSelf,0)=1 and ISNULL(b.SubmitRT,0)=0
     -- 异常流程
     If @@Error<>0
     Goto ErrM

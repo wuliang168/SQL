d020b6cfd7b33745d7efcf6b1d4ac35eeea7e865
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-06-28 09:34:53 +0800

    Update:合规检查临时新增2018年度考核视图

--- zszq_SQL/嘉扬/01 薪资福利/01 年度绩效考核/pVW_pYear_Score_bk.sql
+++ zszq_SQL/嘉扬/01 薪资福利/01 年度绩效考核/pVW_pYear_Score_bk.sql
@@ -1,16 +1,9 @@
--- pVW_pYear_Score
+-- pVW_pYear_Score_bk
 ---- pYear_Score
 select EID,pYear_ID,Score_Status,Score_Type1,Score_Type2,Score_EID,Score_DepID,Score1,Score2,Score3,Score4,Score5,Score6,Score7,Score8,
 ScoreTotal,ScoreEach,ScoreSTG1,ScoreSTG2,ScoreSTG3,ScoreSTG4,ScoreSTG5,ScoreCompl,ScoreParty,Weight1,Weight2,Weight3,Modulus,
 TotalNum,TotalRankNum,ScoreYear,isRanking,Ranking,RankLevel,Remark
 from pYear_Score
-
----- pYear_Score_all
---UNION
---select EID,pYear_ID,Score_Status,Score_Type1,Score_Type2,Score_EID,Score_DepID,Score1,Score2,Score3,Score4,Score5,Score6,Score7,Score8,
---ScoreTotal,ScoreEach,ScoreSTG1,ScoreSTG2,ScoreSTG3,ScoreSTG4,ScoreSTG5,ScoreCompl,ScoreParty,Weight1,Weight2,Weight3,Modulus,
---TotalNum,TotalRankNum,ScoreYear,isRanking,Ranking,RankLevel,Remark
---from pYear_Score_all
 
 ---- pYear_Score_bk
 UNION

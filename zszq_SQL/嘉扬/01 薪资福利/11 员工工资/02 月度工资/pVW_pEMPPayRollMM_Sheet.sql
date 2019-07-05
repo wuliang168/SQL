88fd8f06242d16d94ffecbbf922acb154c2e2fae
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-07-05 08:43:00 +0800

    Update: 更新输出格式化表格查询的表格

--- zszq_SQL/嘉扬/01 薪资福利/11 员工工资/02 月度工资/pVW_pEMPPayRollMM_Sheet.sql
+++ zszq_SQL/嘉扬/01 薪资福利/11 员工工资/02 月度工资/pVW_pEMPPayRollMM_Sheet.sql
@@ -1,14 +1,14 @@
 -- pVW_pEMPPayRollMM_Sheet
 
 -- 员工明细
-select a.PayRollMonth as PayRollMonth,d.DepAbbr as DepAbbr,b.SalaryPayID as SalaryPayID,a.EID as EID,a.HRLID as HRLID,c.Name as Name,a.SalaryTotal as SalaryTotal,
+select a.PayRollMonth as PayRollMonth,d.DepAbbr as DepAbbr,b.SalaryPayID as SalaryPayID,a.EID as EID,a.EMP_Code as HRLID,c.Name as Name,a.SalaryTotal as SalaryTotal,
 a.BackPayBTTotal as BackPayBTTotal,a.AllowanceBTTotal as AllowanceBTTotal,a.FestivalFeeBTTotal as FestivalFeeBTTotal,a.GeneralBonus as GeneralBonus,
 a.DeductionBTTotal as DeductionBTTotal,a.TotalPayAmount as TotalPayAmount,a.OneTimeAnnualBonus as OneTimeAnnualBonus,
 (ISNULL(a.HousingFundEMP,0)+ISNULL(a.EndowInsEMP,0)+ISNULL(a.MedicalInsEMP,0)+ISNULL(a.UnemployInsEMP,0)+ISNULL(a.FundInsEMPPlusTotal,0)) as FundInsEMP,
 a.PITSpclMinusTotal as PITSpclMinusTotal,a.TotalTaxAmount as TotalTaxAmount,a.PersonalIncomeTax as PersonalIncomeTax,a.OneTimeAnnualBonusTax as OneTimeAnnualBonusTax,
 a.AllowanceATTotal as AllowanceATTotal,a.DeductionATTotal as DeductionATTotal,a.TaxFeeReturnAT as TaxFeeReturnAT,
 a.PensionEMPBT as PensionEMPBT,a.PensionEMP as PensionEMP,a.FinalPayingAmount as FinalPayingAmount,d.xOrder as DepxOrder,0 as NamexOrder,e.Xorder as JobXorder
-from pVW_pEMPPayRollCurMM a,pEmployeeEmolu b,eEmployee c,oDepartment d,oJob e
+from pEMPPayRollPerMM a,pEmployeeEmolu b,eEmployee c,oDepartment d,oJob e
 where a.EID=b.EID and a.EID=c.EID and dbo.eFN_getdepid(c.DepID)=d.DepID and c.JobID=e.JobID
 
 -- 部门汇总
@@ -18,7 +18,7 @@ SUM(a.GeneralBonus),SUM(a.DeductionBTTotal),SUM(a.TotalPayAmount),SUM(a.OneTimeA
 SUM(ISNULL(a.HousingFundEMP,0)+ISNULL(a.EndowInsEMP,0)+ISNULL(a.MedicalInsEMP,0)+ISNULL(a.UnemployInsEMP,0)+ISNULL(a.FundInsEMPPlusTotal,0)),
 SUM(a.PITSpclMinusTotal),SUM(a.TotalTaxAmount),SUM(a.PersonalIncomeTax),SUM(a.OneTimeAnnualBonusTax),
 SUM(a.AllowanceATTotal),SUM(a.DeductionATTotal),SUM(a.TaxFeeReturnAT),SUM(a.PensionEMPBT),SUM(a.PensionEMP),SUM(a.FinalPayingAmount),d.xOrder,1,NULL
-from pVW_pEMPPayRollCurMM a,pEmployeeEmolu b,eEmployee c,oDepartment d
+from pEMPPayRollPerMM a,pEmployeeEmolu b,eEmployee c,oDepartment d
 where a.EID=b.EID and a.EID=c.EID and dbo.eFN_getdepid(c.DepID)=d.DepID
 group by a.PayRollMonth,d.DepAbbr,b.SalaryPayID,d.xOrder
 
@@ -29,6 +29,6 @@ SUM(a.DeductionBTTotal),SUM(a.TotalPayAmount),SUM(a.OneTimeAnnualBonus),
 SUM(ISNULL(a.HousingFundEMP,0)+ISNULL(a.EndowInsEMP,0)+ISNULL(a.MedicalInsEMP,0)+ISNULL(a.UnemployInsEMP,0)+ISNULL(a.FundInsEMPPlusTotal,0)),
 SUM(a.PITSpclMinusTotal),SUM(a.TotalTaxAmount),SUM(a.PersonalIncomeTax),SUM(a.OneTimeAnnualBonusTax),
 SUM(a.AllowanceATTotal),SUM(a.DeductionATTotal),SUM(a.TaxFeeReturnAT),SUM(a.PensionEMPBT),SUM(a.PensionEMP),SUM(a.FinalPayingAmount),99999999999,2,NULL
-from pVW_pEMPPayRollCurMM a,pEmployeeEmolu b,eEmployee c,oDepartment d
+from pEMPPayRollPerMM a,pEmployeeEmolu b,eEmployee c,oDepartment d
 where a.EID=b.EID and a.EID=c.EID and dbo.eFN_getdepid(c.DepID)=d.DepID
 group by b.SalaryPayID

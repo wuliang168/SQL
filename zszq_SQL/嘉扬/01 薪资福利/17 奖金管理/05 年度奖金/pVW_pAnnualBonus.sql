bc9f5364221dabe360dcf7e4996d0fcf701dcf3f
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-07-09 11:20:24 +0800

    Update:更新年度奖金查看视图

--- zszq_SQL/嘉扬/01 薪资福利/17 奖金管理/05 年度奖金/pVW_pAnnualBonus.sql
+++ zszq_SQL/嘉扬/01 薪资福利/17 奖金管理/05 年度奖金/pVW_pAnnualBonus.sql
@@ -1,9 +1,10 @@
 -- pVW_pAnnualBonus
+
 select a.Year as Year,a.Date as Date,a.AnnualBonusDepID as AnnualBonusDepID,a.EMPDepID as EMPDepID,
-a.EID as EID,a.Identification as Identification,a.AnnualBonusType as AnnualBonusType,a.AnnualBonus as AnnualBonus
+a.EID as EID,a.BID as BID,a.Identification as Identification,a.AnnualBonusType as AnnualBonusType,a.AnnualBonus as AnnualBonus
 from pYear_AnnualBonus a
 
 UNION ALL
 select a.Year as Year,a.Date as Date,a.AnnualBonusDepID as AnnualBonusDepID,a.EMPDepID as EMPDepID,
-a.EID as EID,a.Identification as Identification,a.AnnualBonusType as AnnualBonusType,a.AnnualBonus as AnnualBonus
+a.EID as EID,a.BID as BID,a.Identification as Identification,a.AnnualBonusType as AnnualBonusType,a.AnnualBonus as AnnualBonus
 from pYear_AnnualBonus_all a

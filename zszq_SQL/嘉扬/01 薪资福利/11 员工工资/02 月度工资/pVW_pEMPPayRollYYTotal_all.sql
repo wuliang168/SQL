0c78a3edbb1e64787432d6644843f1b88a26d153
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-07-19 17:39:12 +0800

    Update:调整工资年度查看内容

--- zszq_SQL/嘉扬/01 薪资福利/11 员工工资/02 月度工资/pVW_pEMPPayRollYYTotal_all.sql
+++ zszq_SQL/嘉扬/01 薪资福利/11 员工工资/02 月度工资/pVW_pEMPPayRollYYTotal_all.sql
@@ -6,6 +6,6 @@ SUM(OneTimeAnnualBonus) as OneTimeAnnualBonusY,SUM(HousingFundEMP) as HousingFun
 SUM(UnemployInsEMP) as UnemployInsEMPY,SUM(FundInsEMPPlusTotal) as FundInsEMPPlusTotalY,SUM(FundInsEMPTotal) as FundInsEMPTotalY,SUM(PITSpclMinusTotal) as PITSpclMinusTotalY,
 SUM(TotalTaxAmount) as TotalTaxAmountY,SUM(PersonalIncomeTax) as PersonalIncomeTaxY,SUM(OneTimeAnnualBonusTax) as OneTimeAnnualBonusTaxY,SUM(AllowanceATTotal) as AllowanceATTotalY,
 SUM(DeductionATTotal) as DeductionATTotalY,SUM(TaxFeeReturnAT) as TaxFeeReturnATY,SUM(PensionEMP) as PensionEMPY,SUM(PensionEMPBT) as PensionEMPBTY,
-SUM(FinalPayingAmount) as FinalPayingAmountY, SUM(TotalPayAmount)+SUM(OneTimeAnnualBonus)+SUM(AllowanceATTotal)+SUM(TaxFeeReturnAT) as PayRollATTotalY
+SUM(FinalPayingAmount) as FinalPayingAmountY, SUM(TotalPayAmount)+SUM(OneTimeAnnualBonus) as PayRollATTotalY
 from PEMPPAYROLLPERMM_ALL
 group by YEAR(PayrollMonth),EID,EMP_Code

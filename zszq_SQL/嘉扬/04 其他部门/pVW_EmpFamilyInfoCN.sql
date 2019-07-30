9afafddeeb112d0f6a12d2783637de8750f06831
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-07-30 08:53:50 +0800

    Update:修改亲属关系为中文显示

--- zszq_SQL/嘉扬/04 其他部门/pVW_EmpFamilyInfoCN.sql
+++ zszq_SQL/嘉扬/04 其他部门/pVW_EmpFamilyInfoCN.sql
@@ -1,7 +1,8 @@
 -- pVW_EmpFamilyInfoCN
 
 SELECT e.xorder AS 排序,g.Account as OA账号,
-a.Badge as 工号, a.Name as 姓名,b.Fname as 亲属姓名,b.CERTID as 亲属证件号码,b.relation as 亲属关系,b.tel as 亲属手机号码
+a.Badge as 工号, a.Name as 姓名,b.Fname as 亲属姓名,b.CERTID as 亲属证件号码,
+(select Title from eCD_Relation where ID=b.relation) as 亲属关系,b.tel as 亲属手机号码
 FROM dbo.eEmployee AS a 
 INNER JOIN ebg_family AS b on a.EID=b.EID
 INNER JOIN dbo.oJob AS e ON a.JobID = e.JobID
--- zszq_SQL/嘉扬/04 其他部门/pVW_EmpFamilyInfoEN.sql
+++ zszq_SQL/嘉扬/04 其他部门/pVW_EmpFamilyInfoEN.sql
@@ -1,7 +1,8 @@
 -- pVW_EmpFamilyInfoEN
 
 SELECT e.xorder AS xOrder,g.Account as OAAccount,
-a.Badge as Badge, a.Name as Name,b.Fname as Fname,b.CERTID as FCertID,b.relation as FRelation,b.tel as FMobile
+a.Badge as Badge, a.Name as Name,b.Fname as Fname,b.CERTID as FCertID,
+(select Title from eCD_Relation where ID=b.relation) as FRelation,b.tel as FMobile
 FROM dbo.eEmployee AS a 
 INNER JOIN ebg_family AS b on a.EID=b.EID
 INNER JOIN dbo.oJob AS e ON a.JobID = e.JobID

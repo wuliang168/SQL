515c151a49370c3ca0f4a9379f717dc17a6cafde
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-08-02 10:19:35 +0800

    Update:调整解密后的密码转移到SkySecUser视图中

--- zszq_SQL/嘉扬/00 公共组件/25 OA/pVW_Employee.sql
+++ zszq_SQL/嘉扬/00 公共组件/25 OA/pVW_Employee.sql
@@ -4,7 +4,7 @@
 select b.xOrder*10000+4010 as JobxOrder,NULL as EID,a.BID as BID,NULL as HRLID,NULL as Badge,a.Identification as Identification,ID as CRMID,a.Name as Name,b.CompID as CompID,
 a.DepID as DepID,dbo.eFN_getdepid1st(a.DepID) as DepID1st,dbo.eFN_getdepid2nd(a.DepID) as DepID2nd,b.DepType as DepType,
 NULL as KPIDepID,b.DepAbbr as DepTitle,NULL as DepProperty1,a.JobTitle as JobTitle,a.Status as Status,NULL as EmpGrade,
-NULL as CompPartTime,a.JoinDate as JoinDate,a.LeaDate as LeaDate,NULL as WorkCity,a.Mobile as Mobile,a.Account as Account,NULL as eHR_PWD
+NULL as CompPartTime,a.JoinDate as JoinDate,a.LeaDate as LeaDate,NULL as WorkCity,a.Mobile as Mobile,a.Account as Account
 from pCRMStaff a,oDepartment b
 where a.Status=1 and a.DepID=b.DepID
 --and a.Identification not in (select a.Certno from eDetails a,eEmployee b where a.EID=b.EID and b.Status in (1,2,3))
@@ -14,8 +14,7 @@ UNION
 select c.xOrder as JobxOrder,a.EID as EID,NULL as BID,a.HRLID as HRLID,a.Badge as Badge,NULL as Identification,NULL as CRMID,a.Name as Name,d.CompID as CompID,
 a.DepID as DepID,dbo.eFN_getdepid1st(a.DepID) as DepID1st,dbo.eFN_getdepid2nd(a.DepID) as DepID2nd,d.DepType as DepType,
 e.kpiDepID as KPIDepID,d.DepAbbr as DepTitle,d.DepProperty1 as DepProperty1,c.Title as JobTitle,a.Status as Status,a.EmpGrade as EmpGrade,
-ISNULL(e.pegroup,e.perole) as CompPartTime,b.JoinDate as JoinDate,b.LeaDate as LeaDate,a.WorkCity as WorkCity,f.Mobile as Mobile,g.Account as Account,
-dbo.skyDeCryptFMBase64String(g.Password,0x6B08315047660D2C) as eHR_PWD
+ISNULL(e.pegroup,e.perole) as CompPartTime,b.JoinDate as JoinDate,b.LeaDate as LeaDate,a.WorkCity as WorkCity,f.Mobile as Mobile,g.Account as Account
 from eEmployee a,eStatus b,oJob c,oDepartment d,pEmployee_register e,eDetails f,SkySecUser g
 where a.EID=b.EID and a.JobID=c.JobID and a.DepID=d.DepID
 and a.EID=e.EID and a.EID=f.EID and a.EID=g.EID

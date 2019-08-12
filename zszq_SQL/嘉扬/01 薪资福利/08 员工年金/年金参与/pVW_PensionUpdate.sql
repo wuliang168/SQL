95e1b51c2668575c3c9024a58a05d20aefca9f2f
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-08-12 16:55:07 +0800

    Update:合并前后台员工信息

--- zszq_SQL/嘉扬/01 薪资福利/08 员工年金/年金参与/pVW_PensionUpdate.sql
+++ zszq_SQL/嘉扬/01 薪资福利/08 员工年金/年金参与/pVW_PensionUpdate.sql
@@ -1,15 +1,8 @@
 -- pVW_PensionUpdate
 
 -- 非投理顾员工
-select a.IsPension as IsPension,b.Name as Name,d.Certno as Identification,b.CompID as CompID,b.DepID1st as DepID1,b.DepID2nd as DepID2,
+select a.IsPension as IsPension,a.EID,a.BID,b.Name as Name,b.identification as Identification,b.CompID as CompID,b.DepID1st as DepID1,b.DepID2nd as DepID2,
 b.DepType as DepType,b.JobTitle as JobTitle,b.Status as Status,b.JoinDate as JoinDate,b.LeaDate as LeaDate,c.SalaryPayID as SalaryPayID,
 b.JobXorder as Xorder
-from pPensionUpdatePerEmp a,pVW_Employee b,pEmployeeEmolu c,eDetails d
-where a.EID=b.EID and a.EID=C.EID and a.EID=d.EID
-
--- 投理顾员工
-UNION
-select a.IsPension as IsPension,a.Name as Name,a.Identification as Identification,11 as CompID,a.SupDepID as DepID1,a.DepID as DepID2,b.DepType as DepTyep,
-(select JobAbbr from oJob where JobID=a.JobID) as JobTitle,a.Status as Status,a.JoinDate as JoinDate,c.LeaveDate as LeaDate,c.SalaryPayID as SalaryPayID,b.Xorder*10000+4030 as Xorder
-from pPensionUpdatePerSDM a,oDepartment b,pSalesDepartMarketerEmolu c
-where ISNULL(a.DepID,a.SupDepID)=b.DepID and a.Identification=c.Identification
+from pPensionUpdatePerEmp a,pVW_Employee b,pEMPSalary c
+where ISNULL(a.EID,a.BID)=ISNULL(b.EID,b.BID) and ISNULL(a.EID,a.BID)=ISNULL(C.EID,C.BID)

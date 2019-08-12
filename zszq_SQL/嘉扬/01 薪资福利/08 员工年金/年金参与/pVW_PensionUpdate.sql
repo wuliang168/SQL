be564a68cba1d9b8823b13f96d1cba64588fec4f
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-08-12 16:56:56 +0800

    Update:新增年金参加年度

--- zszq_SQL/嘉扬/01 薪资福利/08 员工年金/年金参与/pVW_PensionUpdate.sql
+++ zszq_SQL/嘉扬/01 薪资福利/08 员工年金/年金参与/pVW_PensionUpdate.sql
@@ -1,8 +1,8 @@
 -- pVW_PensionUpdate
 
 -- 非投理顾员工
-select a.IsPension as IsPension,a.EID,a.BID,b.Name as Name,b.identification as Identification,b.CompID as CompID,b.DepID1st as DepID1,b.DepID2nd as DepID2,
-b.DepType as DepType,b.JobTitle as JobTitle,b.Status as Status,b.JoinDate as JoinDate,b.LeaDate as LeaDate,c.SalaryPayID as SalaryPayID,
-b.JobXorder as Xorder
+select a.PensionYear as PensionYear,a.IsPension as IsPension,a.EID as EID,a.BID as BID,b.Name as Name,b.identification as Identification,
+b.CompID as CompID,b.DepID1st as DepID1,b.DepID2nd as DepID2,b.DepType as DepType,b.JobTitle as JobTitle,b.Status as Status,
+b.JoinDate as JoinDate,b.LeaDate as LeaDate,c.SalaryPayID as SalaryPayID,b.JobXorder as Xorder
 from pPensionUpdatePerEmp a,pVW_Employee b,pEMPSalary c
 where ISNULL(a.EID,a.BID)=ISNULL(b.EID,b.BID) and ISNULL(a.EID,a.BID)=ISNULL(C.EID,C.BID)

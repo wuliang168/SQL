2f4364318ebde7cbd541b018aadd854f5c1c98c4
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-08-13 13:49:27 +0800

    Update:添加新增前台员工功能

---
+++ zszq_SQL/嘉扬/01 薪资福利/08 员工年金/年金参与/eSP_pPensionUpdateAddCRM.sql
@@ -0,0 +1,54 @@
+USE [zszq]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER  Procedure [dbo].[eSP_pPensionUpdateAddCRM]
+-- skydatarefresh eSP_pPensionUpdateAddCRM
+    @leftid int,
+    @BID int,
+    @RetVal int=0 Output
+As
+/*
+-- Create By wuliang E004205
+-- 年金新增前台员工程序
+-- @leftid为前台员工对应的部门ID
+-- @BID 为年金参与前台员工的ID
+*/
+Begin
+
+    -- 员工已经存在，不可重复添加
+    If Exists(Select 1 From pPensionUpdatePerEmp Where BID=@BID AND ISNULL(IsClosed,0)=0)
+    Begin
+        Set @RetVal = 1100001
+        Return @RetVal
+    End
+
+
+    Begin TRANSACTION
+
+
+    -- 新增前台员工
+    insert into pPensionUpdatePerEmp(PensionYear,BID)
+    values ((select PensionYear from pPensionUpdatePerDep where ISNULL(DepID,SupDepID)=@leftid and ISNULL(IsClosed,0)=0 and ISNULL(IsSubmit,0)=0),
+    @BID)
+    -- 异常流程
+    If @@Error<>0
+    Goto ErrM
+
+
+    -- 递交
+    COMMIT TRANSACTION
+
+    -- 正常处理流程
+    Set @Retval = 0
+    Return @Retval
+
+    -- 异常处理流程
+    ErrM:
+    ROLLBACK TRANSACTION
+    Set @Retval = -1
+    Return @Retval
+
+End

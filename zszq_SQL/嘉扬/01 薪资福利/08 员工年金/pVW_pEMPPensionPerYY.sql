3d5be5173f5072c97e27dc851a99780de973b74c
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-08-15 11:21:38 +0800

    Update:调整企业年金部分功能

--- zszq_SQL/嘉扬/01 薪资福利/08 员工年金/pVW_pEMPPensionPerYY.sql
+++ zszq_SQL/嘉扬/01 薪资福利/08 员工年金/pVW_pEMPPensionPerYY.sql
@@ -1,17 +1,13 @@
 -- pVW_pEMPPensionPerYY
-select a.PensionYear,a.Badge,a.Name,e.CertNo,a.IsPension,a.JoinDate,a.LeaDate,a.PostModulusPerYY,a.PostMonthPerYY,
-a.PostModulusPerYY*a.PostMonthPerYY as PostModulusPerMM,
-d.PensionYearTotal*a.PostModulusPerYY*a.PostMonthPerYY/
-((select SUM(PostModulusPerYY*PostMonthPerYY) from pVW_pEMPPostModulusPerYY where Year(PensionYear)=Year(d.PensionYear))+
-(select SUM(PostModulusPerYY*PostMonthPerYY) from pVW_pSDMarketerPostModulusPerYY where Year(PensionYear)=Year(d.PensionYear))) as GrpPensionPerYY,
-(case when b.Status not in (4,5) then d.PensionYearTotal*a.PostModulusPerYY*a.PostMonthPerYY/
-((select SUM(PostModulusPerYY*PostMonthPerYY) from pVW_pEMPPostModulusPerYY where Year(PensionYear)=Year(d.PensionYear))+
-(select SUM(PostModulusPerYY*PostMonthPerYY) from pVW_pSDMarketerPostModulusPerYY where Year(PensionYear)=Year(d.PensionYear)))/4
-else NULL END) as EmpPensionPerYY,
-c.xorder as jobxorder
+
+select a.PensionYear,a.EID as EID,a.BID as BID,a.Badge,a.Name,b.Identification as CertNo,a.IsPension,a.JoinDate,a.LeaDate,a.PostModulusPerYY,a.PostMonthPerYY,
+a.PostModulusPerYY*a.PostMonthPerYY as PostModulusPerMM,d.PensionYearTotal,
+ROUND(d.PensionYearTotal*a.PostModulusPerYY*a.PostMonthPerYY/e.ModulusTotalYY,2) as GrpPensionPerYY,
+(case when DATEDIFF(YY,ISNULL(a.LeaDate,dateAdd(yy,1,CONVERT(char(5),a.PensionYear, 120) + '12-31')),a.PensionYear)<0 and a.Status<>5 
+then ROUND(d.PensionYearTotal*a.PostModulusPerYY*a.PostMonthPerYY/e.ModulusTotalYY/4,2) else NULL END) as EmpPensionPerYY,
+B.JobxOrder as jobxorder
 from pVW_pEMPPostModulusPerYY as a
-inner join eemployee as b on a.Badge=b.Badge
-inner join oJob as c on b.JobID=c.JobID
-left join pPensionPerYY as d on DATEDIFF(yy,a.PensionYear,d.PensionYear)=0
-inner join eDetails as e on b.EID=e.EID
-where ISNULL(a.PostModulusPerYY,0) <> 0
+inner join pVW_Employee as b on ISNULL(a.EID,a.BID)=ISNULL(b.EID,b.BID)
+inner join pPensionPerYY as d on DATEDIFF(yy,a.PensionYear,d.PensionYear)=0
+inner join (select PensionYear,SUM(PostModulusPerYY*PostMonthPerYY) as ModulusTotalYY from pVW_pEMPPostModulusPerYY where ISNULL(IsPension,0)=1 group by PensionYear) as e on DATEDIFF(yy,a.PensionYear,e.PensionYear)=0
+where ISNULL(a.IsPension,0)=1
--- zszq_SQL/嘉扬/01 薪资福利/08 员工年金/pVW_pEMPPostModulusPerYY.sql
+++ zszq_SQL/嘉扬/01 薪资福利/08 员工年金/pVW_pEMPPostModulusPerYY.sql
@@ -1,42 +1,40 @@
 -- pVW_pEMPPostModulusPerYY
-select a.PensionYear as PensionYear,f.badge as Badge,f.Name as Name,b.IsPension as IsPension,c.JoinDate as JoinDate,c.LeaDate as LeaDate,
-b.LastYearMDID as LastYearMDID,b.LastYearAdminID as LastYearAdminID,
+
+select a.PensionYear as PensionYear,g.EID as EID,g.BID as BID,f.badge as Badge,f.Name as Name,g.IsPension as IsPension,g.JoinDate as JoinDate,g.LeaDate as LeaDate,
+g.Status as Status,g.MDIDYY as LastYearMDID,g.AdminIDYY as LastYearAdminID,
 /* 入职日期大于或者退休日期小于年金分配日期 */
-(case when YEAR(c.JoinDate)>YEAR(a.PensionYear) OR YEAR(ISNULL(c.LeaDate,dateAdd(yy,1,CONVERT(char(5), a.PensionYear, 120) + '12-31')))<YEAR(a.PensionYear) 
-then NULL 
+(case when YEAR(g.JoinDate)>YEAR(a.PensionYear) OR YEAR(ISNULL(g.LeaDate,dateAdd(yy,1,CONVERT(char(5), a.PensionYear, 120) + '12-31')))<YEAR(a.PensionYear) 
+then 0 
 else (case when ISNULL(d.AdminModulus,0)>ISNULL(e.MDModulus,0) then ISNULL(d.AdminModulus,0) else ISNULL(e.MDModulus,0) end) end) as PostModulusPerYY,
 (case 
 /* 入职日期大于或者退休日期小于年金分配日期 */
-when YEAR(c.JoinDate)>YEAR(a.PensionYear) OR YEAR(ISNULL(c.LeaDate,dateAdd(yy,1,CONVERT(char(5), a.PensionYear, 120) + '12-31')))<YEAR(a.PensionYear)
-then NULL
+when YEAR(g.JoinDate)>YEAR(a.PensionYear) OR YEAR(ISNULL(g.LeaDate,dateAdd(yy,1,CONVERT(char(5), a.PensionYear, 120) + '12-31')))<YEAR(a.PensionYear)
+then 0
 /* 入职日期小于或者退休日期大于年金分配日期 */
-when YEAR(c.JoinDate)<YEAR(a.PensionYear) AND YEAR(ISNULL(c.LeaDate,dateAdd(yy,1,CONVERT(char(5), a.PensionYear, 120) + '12-31')))>YEAR(a.PensionYear) 
+when YEAR(g.JoinDate)<YEAR(a.PensionYear) AND YEAR(ISNULL(g.LeaDate,dateAdd(yy,1,CONVERT(char(5), a.PensionYear, 120) + '12-31')))>YEAR(a.PensionYear) 
 then 12
 /* 入职日期小于并且退休日期等于年金分配日期 */
-when YEAR(c.JoinDate)<YEAR(a.PensionYear) AND YEAR(ISNULL(c.LeaDate,dateAdd(yy,1,CONVERT(char(5), a.PensionYear, 120) + '12-31')))=YEAR(a.PensionYear)
+when YEAR(g.JoinDate)<YEAR(a.PensionYear) AND YEAR(ISNULL(g.LeaDate,dateAdd(yy,1,CONVERT(char(5), a.PensionYear, 120) + '12-31')))=YEAR(a.PensionYear)
 then 
-(case when DAY(c.LeaDate)>(32-DAY(c.LeaDate+32-DAY(ISNULL(c.LeaDate,dateAdd(yy,1,CONVERT(char(5), a.PensionYear, 120) + '12-31')))))/2 
-then MONTH(c.LeaDate) else MONTH(c.LeaDate)-1 end)
+(case when DAY(g.LeaDate)>(32-DAY(g.LeaDate+32-DAY(ISNULL(g.LeaDate,dateAdd(yy,1,CONVERT(char(5), a.PensionYear, 120) + '12-31')))))/2 
+then MONTH(g.LeaDate) else MONTH(g.LeaDate)-1 end)
 /* 入职日期等于并且退休日期大于年金分配日期 */
-when YEAR(c.JoinDate)=YEAR(a.PensionYear) AND YEAR(ISNULL(c.LeaDate,dateAdd(yy,1,CONVERT(char(5), a.PensionYear, 120) + '12-31')))>YEAR(a.PensionYear)
+when YEAR(g.JoinDate)=YEAR(a.PensionYear) AND YEAR(ISNULL(g.LeaDate,dateAdd(yy,1,CONVERT(char(5), a.PensionYear, 120) + '12-31')))>YEAR(a.PensionYear)
 then 
-(case when DAY(c.JoinDate)>(32-DAY(c.JoinDate+32-DAY(c.JoinDate)))/2 then 12-MONTH(c.JoinDate) else 12-MONTH(c.JoinDate)+1 end)
+(case when DAY(g.JoinDate)>(32-DAY(g.JoinDate+32-DAY(g.JoinDate)))/2 then 12-MONTH(g.JoinDate) else 12-MONTH(g.JoinDate)+1 end)
 /* 入职日期等于并且退休日期等于年金分配日期 */
-when YEAR(c.JoinDate)=YEAR(a.PensionYear) AND YEAR(ISNULL(c.LeaDate,dateAdd(yy,1,CONVERT(char(5), a.PensionYear, 120) + '12-31')))=YEAR(a.PensionYear)
+when YEAR(g.JoinDate)=YEAR(a.PensionYear) AND YEAR(ISNULL(g.LeaDate,dateAdd(yy,1,CONVERT(char(5), a.PensionYear, 120) + '12-31')))=YEAR(a.PensionYear)
 then 
-(case when DAY(c.JoinDate)>(32-DAY(c.JoinDate+32-DAY(c.JoinDate)))/2 and DAY(c.LeaDate)>(32-DAY(c.LeaDate+32-DAY(c.LeaDate)))/2
-then MONTH(c.LeaDate)-MONTH(c.JoinDate)
-when DAY(c.JoinDate)>(32-DAY(c.JoinDate+32-DAY(c.JoinDate)))/2 and DAY(c.LeaDate)<(32-DAY(c.LeaDate+32-DAY(c.LeaDate)))/2
-then MONTH(c.LeaDate)-MONTH(c.JoinDate)-1
-when DAY(c.JoinDate)<(32-DAY(c.JoinDate+32-DAY(c.JoinDate)))/2 and DAY(c.LeaDate)>(32-DAY(c.LeaDate+32-DAY(c.LeaDate)))/2
-then MONTH(c.LeaDate)-MONTH(c.JoinDate)+1
-else MONTH(c.LeaDate)-MONTH(c.JoinDate) end)
+(case when DAY(g.JoinDate)>(32-DAY(g.JoinDate+32-DAY(g.JoinDate)))/2 and DAY(g.LeaDate)>(32-DAY(g.LeaDate+32-DAY(g.LeaDate)))/2
+then MONTH(g.LeaDate)-MONTH(g.JoinDate)
+when DAY(g.JoinDate)>(32-DAY(g.JoinDate+32-DAY(g.JoinDate)))/2 and DAY(g.LeaDate)<(32-DAY(g.LeaDate+32-DAY(g.LeaDate)))/2
+then MONTH(g.LeaDate)-MONTH(g.JoinDate)-1
+when DAY(g.JoinDate)<(32-DAY(g.JoinDate+32-DAY(g.JoinDate)))/2 and DAY(g.LeaDate)>(32-DAY(g.LeaDate+32-DAY(g.LeaDate)))/2
+then MONTH(g.LeaDate)-MONTH(g.JoinDate)+1
+else MONTH(g.LeaDate)-MONTH(g.JoinDate) end)
 end) as PostMonthPerYY
 from pPensionPerYY as a
-left join pEmployeeEmolu as b on ISNULL(b.IsPension,0)=1
-left join eStatus as c on c.EID=b.EID 
-left join oCD_AdminType as d on b.LastYearAdminID=d.ID
-left join oCD_MDType as e on b.LastYearMDID=e.ID
-inner join eemployee as f on b.eid=f.eid and f.status not in (4)
-left join oJob as g on f.JobID=g.JobID
-where ISNULL(a.Closed,0)=0 and ISNULL(a.Submit,0)=1
+inner join pPensionUpdatePerEmp as g on DATEDIFF(YY,a.PensionYear,g.PensionYear)=0 and ISNULL(g.IsSubmit,0)=1
+inner join pVW_employee as f on ISNULL(g.EID,g.BID)=ISNULL(f.eid,f.BID)
+left join oCD_AdminType as d on g.AdminIDYY=d.ID
+left join oCD_MDType as e on g.MDIDYY=e.ID

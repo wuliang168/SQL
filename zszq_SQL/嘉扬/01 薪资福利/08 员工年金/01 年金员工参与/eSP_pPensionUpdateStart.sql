e2f57e4c741823045105efca0c9f083413d5a496
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-08-16 16:40:32 +0800

    Update:优化报名开启流程

--- zszq_SQL/嘉扬/01 薪资福利/08 员工年金/01 年金员工参与/eSP_pPensionUpdateStart.sql
+++ zszq_SQL/嘉扬/01 薪资福利/08 员工年金/01 年金员工参与/eSP_pPensionUpdateStart.sql
@@ -33,11 +33,11 @@ Begin
     End
 
     -- 上次年金参与未关闭!
-    If Exists(Select 1 From pPensionUpdate Where @ID>1 and ID=@ID-1 And Isnull(Closed,0)=0)
-    Begin
-        Set @RetVal = 930075
-        Return @RetVal
-    End
+    --If Exists(Select 1 From pPensionUpdate Where @ID>1 and ID=@ID-1 And Isnull(Closed,0)=0)
+    --Begin
+    --    Set @RetVal = 930075
+    --    Return @RetVal
+    --End
 
 
     Begin TRANSACTION
@@ -45,55 +45,36 @@ Begin
 
     -- 插入后台员工参与年金pPensionUpdatePerEmp
     ---- 在职
-    ------ 前一年度参加企业年金
-    insert into pPensionUpdatePerEmp(PensionYear,EID,IsPension,IsSubmit,IsClosed)
-    select a.PensionYear,b.EID,1,NULL,NULL
-    from pPensionUpdate a,eEmployee b,eStatus c
-    where a.ID=@ID and b.Status not in (4,5) and b.EID=c.EID and DateDiff(yy,c.JoinDate,a.PensionYear)>=0
-    and b.Badge in (select Badge from pEMPPensionPerYY where DATEDIFF(yy,PensionYear,a.PensionYear)=1)
+    insert into pPensionUpdatePerEmp(PensionYear,EID,IsPension)
+    select a.PensionYear,b.EID,1
+    from pPensionUpdate a,pVW_Employee b,eStatus c
+    where a.ID=@ID and b.Status in (1,2,3) and b.EID=c.EID and b.EID is not NULL and DATEDIFF(yy,c.JoinDate,a.PensionYear)>=0
     -- 异常流程
     If @@Error<>0
     Goto ErrM
-    ------ 前一年度未参加企业年金
-    insert into pPensionUpdatePerEmp(PensionYear,EID,IsPension,IsSubmit,IsClosed)
-    select a.PensionYear,b.EID,NULL,NULL,NULL
-    from pPensionUpdate a,eEmployee b,eStatus c
-    where a.ID=@ID and b.Status not in (4,5) and b.EID=c.EID and DateDiff(yy,c.JoinDate,a.PensionYear)>=0
-    and b.Badge not in (select Badge from pEMPPensionPerYY where DATEDIFF(yy,PensionYear,a.PensionYear)=1)
+    ------ 根据年金分配上年度更新员工报名状态
+    update b
+    set b.IsPension=1
+    from pPensionUpdate a,pPensionUpdatePerEmp b
+    where a.ID=@ID and DATEDIFF(YY,a.PensionYear,b.PensionYear)=0
+    AND b.EID in (select EID from pPensionUpdatePerEmp 
+	where DATEDIFF(yy,PensionYear,(select MIN(PensionYear) from pPensionUpdate where ISNULL(Closed,0)=0))=1 and EID is not NULL and ISNULL(IsPension,0)=1)
     -- 异常流程
     If @@Error<>0
     Goto ErrM
     ---- 退休
-    insert into pPensionUpdatePerEmp(PensionYear,EID,IsPension,IsSubmit,IsClosed)
-    select a.PensionYear,b.EID,1,1,NULL
-    from pPensionUpdate a,eEmployee b,eStatus c
-    where a.ID=@ID and b.Status=5 and b.EID=c.EID
-    and DateDiff(yy,c.LeaDate,a.PensionYear)=0 and c.LeaType=4
+    insert into pPensionUpdatePerEmp(PensionYear,EID,IsPension)
+    select a.PensionYear,b.EID,1
+    from pPensionUpdate a,pVW_Employee b,eStatus c
+    where a.ID=@ID and b.Status=5 and b.EID=c.EID and DateDiff(yy,c.LeaDate,a.PensionYear)<=0
     -- 异常流程
     If @@Error<>0
     Goto ErrM
 
-    -- 插入前台员工参与年金pPensionUpdatePerSDM
-    ---- 在职
-    insert into pPensionUpdatePerSDM(PensionYear,BID,Name,Identification,IsPension,IsSubmit,IsClosed)
-    select a.PensionYear,b.BID,b.Name,b.Identification,NULL,NULL,NULL
-    from pPensionUpdate a,pCRMStaff b
-    where a.ID=@ID and b.Status not in (4,5) and DateDiff(yy,b.JoinDate,a.PensionYear)>=0
-    -- 异常流程
-    If @@Error<>0
-    Goto ErrM
-    ---- 退休
-    insert into pPensionUpdatePerSDM(PensionYear,BID,Name,Identification,IsPension,IsSubmit,IsClosed)
-    select a.PensionYear,b.BID,b.Name,b.Identification,1,NULL,NULL
-    from pPensionUpdate a,pCRMStaff b
-    where a.ID=@ID and b.Status=5 and DateDiff(yy,b.JoinDate,a.PensionYear)>=0
-    -- 异常流程
-    If @@Error<>0
-    Goto ErrM
 
     -- 插入分支机构参与年金pPensionUpdatePerDep
-    insert into pPensionUpdatePerDep(PensionYear,SupDepID,DepID,PensionContact,IsSubmit)
-    select a.PensionYear,b.SupDepID,b.DepID,b.PensionContact,NULL
+    insert into pPensionUpdatePerDep(PensionYear,SupDepID,DepID,PensionContact)
+    select a.PensionYear,b.SupDepID,b.DepID,b.PensionContact
     from pPensionUpdate a,pVW_DepPensionContact b
     where a.ID=@ID and b.SupDepID is not NULL and b.PensionContact is not NULL
     -- 异常流程

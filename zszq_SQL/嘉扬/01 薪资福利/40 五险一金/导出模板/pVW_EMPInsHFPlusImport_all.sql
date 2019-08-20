12a99617476a55f8fdcc9ada1a66c8f04eebda7d
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-08-20 12:08:15 +0800

    Update:优化五险一金导出视图，其中正常缴费显示速度提升明显

--- zszq_SQL/嘉扬/01 薪资福利/40 五险一金/导出模板/pVW_EMPInsHFPlusImport_all.sql
+++ zszq_SQL/嘉扬/01 薪资福利/40 五险一金/导出模板/pVW_EMPInsHFPlusImport_all.sql
@@ -1,4 +1,5 @@
 -- pVW_EMPInsHFPlusImport_all
+
 select b.Month as Month,a.EID as EID,a.HRLID as HRLID,d.chn_name as Name,f.SalaryPayID as SalaryPayID,g.EMPInsuranceDepart as EMPInsuranceDepart,h.EMPHousingFundDepart as EMPHousingFundDepart,
 (select IsSubmit from pEMPInsuranceHousingFundDep where g.EMPInsuranceDepart=ISNULL(DepID2nd,DepID1st) and MONTH=c.Month) as InsuranceIsSubmit,
 (select IsSubmit from pEMPInsuranceHousingFundDep where h.EMPHousingFundDepart=ISNULL(DepID2nd,DepID1st) and MONTH=b.Month) as HousingFundIsSubmit,
@@ -6,9 +7,9 @@ c.EndowInsGRPPlus as SB_C8,c.EndowInsEMPPlus as SB_06,b.HousingFundGRPPlus as SB
 c.MedicalInsGRPPlus as SB_C10,c.UnemployInsGRPPlus as SB_C11,c.MedicalInsEMPPlus as SB_10,c.MaternityInsGRPPlus as SB_C12,c.InjuryInsGRPPlus as SB_C13
 from eEmployee a
 left join pEMPHousingFundPerMM_all b on a.EID=b.EID
-left join pEMPInsurancePerMM_all c on a.EID=c.EID and b.Month=c.Month
+left join pEMPInsurancePerMM_all c on a.EID=c.EID
 left join OPENQUERY(HRLINK, 'SELECT emp_code,emp_id,chn_name FROM DBO.EMP_INFO_MASTER') d on d.emp_code=a.HRLID COLLATE Latin1_General_CI_AI
 inner join pEmployeeEmolu f on a.EID=f.EID
 inner join pEMPInsurance g on a.EID=g.EID
 inner join pEMPHousingFund h on a.EID=h.EID
-where a.status not in (4,5)
+where a.status not in (4,5) and b.Month=c.Month
--- zszq_SQL/嘉扬/01 薪资福利/40 五险一金/导出模板/pVW_EMPInsuranceHousingFundImport_all.sql
+++ zszq_SQL/嘉扬/01 薪资福利/40 五险一金/导出模板/pVW_EMPInsuranceHousingFundImport_all.sql
@@ -1,17 +1,17 @@
 -- pVW_EMPInsuranceHousingFundImport_all
+
 select b.Month as Month,a.EID as EID,a.HRLID as HRLID,d.chn_name as Name,f.SalaryPayID as SalaryPayID,g.EMPInsuranceDepart as EMPInsuranceDepart,h.EMPHousingFundDepart as EMPHousingFundDepart,
 (select IsSubmit from pEMPInsuranceHousingFundDep where g.EMPInsuranceDepart=ISNULL(DepID2nd,DepID1st) and Month=c.Month) as InsuranceIsSubmit,
 (select IsSubmit from pEMPInsuranceHousingFundDep where h.EMPHousingFundDepart=ISNULL(DepID2nd,DepID1st) and Month=b.Month) as HousingFundIsSubmit,
-e.gongjijinmax as gongjijinmax,c.EndowInsEMP as yanglaogeren,c.UnemployInsGRP as shiyegongsi,c.EndowInsGRP as yanglaogongsi,c.UnemployInsEMP as shiyegeren, 
+d.gongjijinmax as gongjijinmax,c.EndowInsEMP as yanglaogeren,c.UnemployInsGRP as shiyegongsi,c.EndowInsGRP as yanglaogongsi,c.UnemployInsEMP as shiyegeren, 
 c.InjuryInsGRP as gongshanggongsi,c.MaternityInsGRP as shengyugongsi,c.MedicalInsGRP as yiliaogongsi,c.MedicalInsEMP as yiliaogeren,
 b.HousingFundGRP as gongjijingongsi,b.HousingFundEMP as gongjijingeren, 
-e.dabing as dabing,e.yanglaoplace as yanglaoplace,e.gongjijinplace as gongjijinplace
+d.dabing as dabing,d.yanglaoplace as yanglaoplace,d.gongjijinplace as gongjijinplace
 from eEmployee a
 left join pEMPHousingFundPerMM_all b on a.EID=b.EID
-left join pEMPInsurancePerMM_all c on a.EID=c.EID and b.Month=c.Month
-left join OPENQUERY(HRLINK, 'SELECT emp_code,emp_id,chn_name FROM DBO.EMP_INFO_MASTER') d on d.emp_code=a.HRLID COLLATE Latin1_General_CI_AI
-left join OPENQUERY(HRLINK, 'SELECT emp_id,dabing,yanglaoplace,gongjijinplace,gongjijinmax FROM DBO.CU_SALARYPLACE') e on d.emp_id=e.emp_id COLLATE Latin1_General_CI_AI
-inner join pEmployeeEmolu f on a.EID=f.EID
+left join pEMPInsurancePerMM_all c on a.EID=c.EID
+left join OPENQUERY(HRLINK, 'SELECT a.emp_code,a.chn_name,b.dabing,b.yanglaoplace,b.gongjijinplace,b.gongjijinmax FROM DBO.EMP_INFO_MASTER a,DBO.CU_SALARYPLACE b WHERE a.emp_id=b.emp_id') d on d.emp_code=a.HRLID COLLATE Latin1_General_CI_AI
+inner join pEMPSalary f on a.EID=f.EID
 inner join pEMPInsurance g on a.EID=g.EID
 inner join pEMPHousingFund h on a.EID=h.EID
-where a.status not in (4,5)
+where a.status not in (4,5) and DATEDIFF(MM,b.Month,c.Month)=0

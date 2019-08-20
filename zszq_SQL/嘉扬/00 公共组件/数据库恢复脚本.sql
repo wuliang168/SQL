f45c1c48a16b6911da18b0ef069b71fa7ec71273
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-08-20 16:32:50 +0800

    Update:数据库内容变更

--- zszq_SQL/嘉扬/00 公共组件/数据库恢复脚本.sql
+++ zszq_SQL/嘉扬/00 公共组件/数据库恢复脚本.sql
@@ -1,5 +1,5 @@
 GO
-Use zszqtest1
+Use zszqtest
 
 --ALTER LOGIN [HRKayang] DISABLE
 GO
@@ -24,17 +24,17 @@ GO
 --Use HRMaster
 GO
 
-Use zszqtest1
+Use zszqtest
 
 GO
 --�������û�ӳ�䵽��½���������µ���Ϣ
 --exec sp_change_users_login 'UPDATE_ONE','HRKayang','HRKayang'
 GO
 Update skySecHResource
-        Set Password='k6mdRv2jrjSTmkO9Mw6e7A==',Server='10.51.190.161',DBName='zszqtest1',Login='HRSYS'
+        Set Password='k6mdRv2jrjSTmkO9Mw6e7A==',Server='10.51.190.161',DBName='zszqtest',Login='HRSYS'
 GO
 GO
-Use zszqtest1
+Use zszqtest
 GO
 --�������û�����ΪDBO
 exec sp_changedbowner HRSYS

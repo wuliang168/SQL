a1e0cbf644c542206ac2d9e9a7711369dd3f817e
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-09-04 08:54:31 +0800

    Update:新增每一行的合计数查看项

--- zszq_SQL/嘉扬/01 薪资福利/11 员工工资/04 工资统计/pVW_pSalaryPerMMPWSumm.sql
+++ zszq_SQL/嘉扬/01 薪资福利/11 员工工资/04 工资统计/pVW_pSalaryPerMMPWSumm.sql
@@ -1,7 +1,7 @@
 -- pVW_pSalaryPerMMPWSumm
 
-select distinct a.ProcessID as ProcessID,a.DepID as DepID,a.SalaryContact as SalaryContact,a.PerMMSummType as PerMMSummType,
-a.EMPType as EMPType,b.EMPPerfSumm as EMPPerfSumm,c.EMPWelfareSumm as EMPWelfareSumm
+select distinct a.ProcessID as ProcessID,a.DepID as DepID,a.SalaryContact as SalaryContact,a.PerMMSummType as PerMMSummType,a.EMPType as EMPType,
+b.EMPPerfSumm as EMPPerfSumm,c.EMPWelfareSumm as EMPWelfareSumm,ISNULL(d.EMPSalary,0)+ISNULL(b.EMPPerfSumm,0)+ISNULL(c.EMPWelfareSumm,0) as EMPSUMMTotalSumm
 from pSalaryPerMMPWSumm a
 left join (select ProcessID,DepID,EMPType,SUM(EMPPerfWelSumm) as EMPPerfSumm from pSalaryPerMMPWSumm 
 where EMPPerfWelType in (select ID from oCD_MonthExpenseType where Title in (N'营销费用',N'项目奖金',N'CRM提成')) group by ProcessID,DepID,EMPType) 
@@ -9,3 +9,5 @@ as b on a.ProcessID=b.ProcessID and a.DepID=b.DepID and a.EMPType=b.EMPType
 left join (select ProcessID,DepID,EMPType,SUM(EMPPerfWelSumm) as EMPWelfareSumm from pSalaryPerMMPWSumm 
 where EMPPerfWelType in (select ID from oCD_MonthExpenseType where Title in (N'过节费',N'高温\取暖费',N'通讯费')) group by ProcessID,DepID,EMPType) 
 as c on a.ProcessID=c.ProcessID and a.DepID=c.DepID and a.EMPType=c.EMPType
+left join pSalaryPerMMSumm_register d on a.ProcessID=d.ProcessID and a.DepID=d.DepID and a.PerMMSummType=d.PerMMSummType and a.EMPType=d.EMPType
+where a.PerMMSummType=1

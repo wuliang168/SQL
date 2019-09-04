0359808b472949b4a30224aabdebd91ee896604b
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-09-04 09:04:50 +0800

    Update:新增将Summ内容填写到register功能

--- zszq_SQL/嘉扬/01 薪资福利/11 员工工资/04 工资统计/eSP_pSalaryPerMMSummBSSubmit.sql
+++ zszq_SQL/嘉扬/01 薪资福利/11 员工工资/04 工资统计/eSP_pSalaryPerMMSummBSSubmit.sql
@@ -6,7 +6,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 ALTER  Procedure [dbo].[eSP_pSalaryPerMMSummBSSubmit]
 -- skydatarefresh eSP_pSalaryPerMMSummBSSubmit
-    @leftid int,  
+    @leftid varchar(20),  
     @RetVal int=0 Output
 As
 /*
@@ -16,36 +16,60 @@ As
 */
 Begin
 
+    declare @DepID int,@ProcessID int
+    set @DepID=convert(int,SUBSTRING(@leftid,0,CHARINDEX('-',@leftid)))
+    set @ProcessID=convert(int,REVERSE(SUBSTRING(REVERSE(@leftid),0,CHARINDEX('-',REVERSE(@leftid)))))
+
     Begin TRANSACTION
 
     -- 更新pSalaryPerMMSumm_register
+    ---- 更新由pVW_pSalaryPerMMPWSumm提供的数据
+    update a
+    set a.EMPPerf=b.EMPPerfSumm,a.EMPWelfare=b.EMPWelfareSumm,a.EMPSUMMTotal=b.EMPSUMMTotalSumm
+    from pSalaryPerMMSumm_register a,pVW_pSalaryPerMMPWSumm b
+    where a.DepID=b.DepID and a.ProcessID=b.ProcessID and a.PerMMSummType=b.PerMMSummType and a.EMPType=b.EMPType
+    and a.DepID=@DepID and a.xOrder<>9 and a.PerMMSummType=1 and a.ProcessID=@ProcessID
+    -- 异常流程
+    If @@Error<>0
+    Goto ErrM
+
     ---- 统计
     update a
-    set a.EMPNum=(select SUM(EMPNum) from pSalaryPerMMSumm_register where xOrder<>9 and DepID=a.DepID and PerMMSummType=1),
-    a.EMPSalary=(select SUM(EMPSalary) from pSalaryPerMMSumm_register where xOrder<>9 and DepID=a.DepID and PerMMSummType=1),
-    a.EMPPerf=(select SUM(EMPPerf) from pSalaryPerMMSumm_register where xOrder<>9 and DepID=a.DepID and PerMMSummType=1),
-    a.EMPWelfare=(select SUM(EMPWelfare) from pSalaryPerMMSumm_register where xOrder<>9 and DepID=a.DepID and PerMMSummType=1),
-    a.EMPPerfLY=(select SUM(EMPPerfLY) from pSalaryPerMMSumm_register where xOrder<>9 and DepID=a.DepID and PerMMSummType=1),
-    a.EMPSUMMTotal=(select SUM(EMPSUMMTotal) from pSalaryPerMMSumm_register where xOrder<>9 and DepID=a.DepID and PerMMSummType=1)
+    set a.EMPNum=(select SUM(EMPNum) from pSalaryPerMMSumm_register where ProcessID=a.ProcessID and xOrder<>9 and DepID=a.DepID and PerMMSummType=1),
+    a.EMPSalary=(select SUM(EMPSalary) from pSalaryPerMMSumm_register where ProcessID=a.ProcessID and xOrder<>9 and DepID=a.DepID and PerMMSummType=1),
+    a.EMPPerf=(select SUM(EMPPerf) from pSalaryPerMMSumm_register where ProcessID=a.ProcessID and xOrder<>9 and DepID=a.DepID and PerMMSummType=1),
+    a.EMPWelfare=(select SUM(EMPWelfare) from pSalaryPerMMSumm_register where ProcessID=a.ProcessID and xOrder<>9 and DepID=a.DepID and PerMMSummType=1),
+    a.EMPPerfLY=(select SUM(EMPPerfLY) from pSalaryPerMMSumm_register where ProcessID=a.ProcessID and xOrder<>9 and DepID=a.DepID and PerMMSummType=1),
+    a.EMPSUMMTotal=(select SUM(EMPSUMMTotal) from pSalaryPerMMSumm_register where ProcessID=a.ProcessID and xOrder<>9 and DepID=a.DepID and PerMMSummType=1)
     from pSalaryPerMMSumm_register a 
-    where a.DepID=@leftid and a.xOrder=9 and a.PerMMSummType=1
+    where a.DepID=@DepID and a.xOrder=9 and a.PerMMSummType=1 and a.ProcessID=@ProcessID
+    -- 异常流程
+    If @@Error<>0
+    Goto ErrM
 
     ---- 预算
     update a
-    set a.EMPNum=(select SUM(EMPNum) from pSalaryPerMMSumm_register where xOrder<>9 and DepID=a.DepID and PerMMSummType=2),
-    a.EMPSalary=(select SUM(EMPSalary) from pSalaryPerMMSumm_register where xOrder<>9 and DepID=a.DepID and PerMMSummType=2),
-    a.EMPPerf=b.EMPPerfSumm,a.EMPWelfare=b.EMPWelfareSumm,
-    a.EMPSUMMTotal=(select SUM(EMPSUMMTotal) from pSalaryPerMMSumm_register where xOrder<>9 and DepID=a.DepID and PerMMSummType=2)
+    set a.EMPNum=(select SUM(EMPNum) from pSalaryPerMMSumm_register where ProcessID=a.ProcessID and xOrder<>9 and DepID=a.DepID and PerMMSummType=2),
+    a.EMPSalary=(select SUM(EMPSalary) from pSalaryPerMMSumm_register where ProcessID=a.ProcessID and xOrder<>9 and DepID=a.DepID and PerMMSummType=2),
+    a.EMPPerf=(select SUM(EMPPerf) from pSalaryPerMMSumm_register where ProcessID=a.ProcessID and xOrder<>9 and DepID=a.DepID and PerMMSummType=2),
+    a.EMPWelfare=(select SUM(EMPWelfare) from pSalaryPerMMSumm_register where ProcessID=a.ProcessID and xOrder<>9 and DepID=a.DepID and PerMMSummType=2),
+    a.EMPSUMMTotal=(select SUM(EMPSUMMTotal) from pSalaryPerMMSumm_register where ProcessID=a.ProcessID and xOrder<>9 and DepID=a.DepID and PerMMSummType=2)
     from pSalaryPerMMSumm_register a,pVW_pSalaryPerMMPWSumm b
-    where a.DepID=@leftid and a.DepID=b.DepID and a.PerMMSummType=b.PerMMSummType and a.PerMMSummType=2
-    and a.xOrder=9
+    where a.DepID=@DepID and a.DepID=b.DepID and a.PerMMSummType=b.PerMMSummType and a.PerMMSummType=2
+    and a.xOrder=9 and a.ProcessID=b.ProcessID and a.ProcessID=@ProcessID
+    -- 异常流程
+    If @@Error<>0
+    Goto ErrM
 
 
     -- 更新pSalaryPerMMSummDep
     update a
     set a.IsSubmit=1,a.SubmitTime=GETDATE()
     from pSalaryPerMMSummDep a 
-    where a.DepID=@leftid and ISNULL(a.IsSubmit,0)=0
+    where a.DepID=@DepID and ISNULL(a.IsSubmit,0)=0 and a.ProcessID=@ProcessID
+    -- 异常流程
+    If @@Error<>0
+    Goto ErrM
 
 
     -- 递交

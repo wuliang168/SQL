9bf5513a0f7f7c07b41f1b275c33863b9b34d0e8
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-09-09 15:56:01 +0800

    Update:新增企业年金补充金额合入分配功能

--- zszq_SQL/嘉扬/01 薪资福利/08 员工年金/00 年金年度分配/pVW_pEMPPensionPerYY.sql
+++ zszq_SQL/嘉扬/01 薪资福利/08 员工年金/00 年金年度分配/pVW_pEMPPensionPerYY.sql
@@ -2,10 +2,10 @@
 
 select a.PensionYear as PensionYear,a.EID as EID,a.BID as BID,a.Badge as Badge,a.Name as Name,b.Identification as CertNo,
 a.IsPension as IsPension,a.JoinDate as JoinDate,a.LeaDate as LeaDate,a.Status as Status,a.PostModulusPerYY as PostModulusPerYY,a.PostMonthPerYY as PostMonthPerYY,
-a.PostModulusPerYY*a.PostMonthPerYY as PostModulusPerMM,d.PensionYearTotal,
-ROUND(d.PensionYearTotal*a.PostModulusPerYY*a.PostMonthPerYY/e.ModulusTotalYY,2) as GrpPensionPerYY,
-(case when DATEDIFF(YY,ISNULL(a.LeaDate,dateAdd(yy,1,CONVERT(char(5),a.PensionYear, 120) + '12-31')),a.PensionYear)<0 and a.Status<>5 
-then ROUND(d.PensionYearTotal*a.PostModulusPerYY*a.PostMonthPerYY/e.ModulusTotalYY/4,2) else NULL END) as EmpPensionPerYY,
+a.PostModulusPerYY*a.PostMonthPerYY as PostModulusPerMM,d.PensionYearTotal as PensionYearTotal,d.PensionYearPlus as PensionYearPlus,
+ROUND((ISNULL(d.PensionYearTotal,0)+ISNULL(d.PensionYearPlus,0))*a.PostModulusPerYY*a.PostMonthPerYY/e.ModulusTotalYY,2) as GrpPensionPerYY,
+(case when DATEDIFF(YY,ISNULL(a.LeaDate,dateAdd(yy,1,CONVERT(char(5),a.PensionYear, 120) + '12-31')),a.PensionYear)<=0 and a.Status<>5 
+then ROUND((ISNULL(d.PensionYearTotal,0)+ISNULL(d.PensionYearPlus,0))*a.PostModulusPerYY*a.PostMonthPerYY/e.ModulusTotalYY/4,2) else NULL END) as EmpPensionPerYY,
 B.JobxOrder as jobxorder
 from pVW_pEMPPostModulusPerYY as a
 inner join pVW_Employee as b on ISNULL(a.EID,a.BID)=ISNULL(b.EID,b.BID)

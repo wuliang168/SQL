a774fe77f7bf5c6bf2e5a679757a3ba81453a14c
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-09-24 17:35:32 +0800

    Update:更新去除在职状态判断

--- zszq_SQL/嘉扬/02 人员发展/24 业绩考核/pVW_TrgtRspCntrReportTo.sql
+++ zszq_SQL/嘉扬/02 人员发展/24 业绩考核/pVW_TrgtRspCntrReportTo.sql
@@ -5,7 +5,7 @@
 select a.EID as EID,a.CompID as CompID,dbo.eFN_getdepid1st(a.DepID) as DepID1st,dbo.eFN_getdepid2nd(a.DepID) as DepID2nd,
 NULL as PreviewTo,dbo.eFN_getdepdirector(dbo.eFN_getdepid1st(a.DepID)) as ReportTo,dbo.eFN_getdepid1st(a.DepID) as ReportToDepID
 from eEmployee a,oDepartment b
-where a.Status not in (4,5) and a.DepID=b.DepID and ISNULL(dbo.eFN_getdepadmin(dbo.eFN_getdepid1st(a.DepID)),0)<>695
+where a.DepID=b.DepID and ISNULL(dbo.eFN_getdepadmin(dbo.eFN_getdepid1st(a.DepID)),0)<>695
 and ISNULL(dbo.eFN_getdepid1st(a.DepID),0)<>361 and a.CompID=11 and dbo.eFN_getdeptype(a.DepID)=1
 and a.Name<>N'胡晶飚'
 
@@ -15,7 +15,7 @@ UNION
 select a.EID as EID,a.CompID as CompID,dbo.eFN_getdepid1st(a.DepID) as DepID1st,dbo.eFN_getdepid2nd(a.DepID) as DepID2nd,
 2098 as PreviewTo,dbo.eFN_getdepdirector(361) as ReportTo,361 as ReportToDepID
 from eEmployee a,oDepartment b
-where a.Status not in (4,5) and a.DepID=b.DepID and ISNULL(dbo.eFN_getdepid1st(a.DepID),0)=361
+where a.DepID=b.DepID and ISNULL(dbo.eFN_getdepid1st(a.DepID),0)=361
 
 ---- 总部(投行)
 ------ 投资银行管理总部：683
@@ -23,7 +23,7 @@ UNION
 select a.EID as EID,a.CompID as CompID,dbo.eFN_getdepid1st(a.DepID) as DepID1st,dbo.eFN_getdepid2nd(a.DepID) as DepID2nd,
 2218 as PreviewTo,dbo.eFN_getdepdirector(683) as ReportTo,695 as ReportToDepID
 from eEmployee a,oDepartment b
-where a.Status not in (4,5) and a.DepID=b.DepID and ISNULL(dbo.eFN_getdepadmin(dbo.eFN_getdepid1st(a.DepID)),0)=695
+where a.DepID=b.DepID and ISNULL(dbo.eFN_getdepadmin(dbo.eFN_getdepid1st(a.DepID)),0)=695
 
 ---- 子公司
 ------ 资管
@@ -31,7 +31,7 @@ UNION
 select a.EID as EID,a.CompID as CompID,dbo.eFN_getdepid1st(a.DepID) as DepID1st,dbo.eFN_getdepid2nd(a.DepID) as DepID2nd,
 NULL as PreviewTo,dbo.eFN_getdepdirector(795) as ReportTo,393 as ReportToDepID
 from eEmployee a,oDepartment b
-where a.Status not in (4,5) and a.DepID=b.DepID
+where a.DepID=b.DepID
 and a.CompID=13
 
 UNION
@@ -40,7 +40,7 @@ UNION
 select a.EID as EID,a.CompID as CompID,dbo.eFN_getdepid1st(a.DepID) as DepID1st,dbo.eFN_getdepid2nd(a.DepID) as DepID2nd,
 1992 as PreviewTo,dbo.eFN_getdepdirector(362) as ReportTo,362 as ReportToDepID
 from eEmployee a,oDepartment b
-where a.Status not in (4,5) and a.DepID=b.DepID and a.EmpGrade in (9,5)
+where a.DepID=b.DepID and a.EmpGrade in (9,5)
 and a.CompID=11 and dbo.eFN_getdeptype(a.DepID) in (2,3)
 
 UNION
@@ -49,5 +49,5 @@ UNION
 select a.EID as EID,a.CompID as CompID,dbo.eFN_getdepid1st(a.DepID) as DepID1st,dbo.eFN_getdepid2nd(a.DepID) as DepID2nd,
 1992 as PreviewTo,dbo.eFN_getdepdirector(362) as ReportTo,362 as ReportToDepID
 from eEmployee a,oDepartment b
-where a.Status not in (4,5) and a.DepID=b.DepID and a.CompID=11
+where a.DepID=b.DepID and a.CompID=11
 and a.Name=N'胡晶飚'

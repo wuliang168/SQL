a1bf2cfdf9666167095b0ae7a06db6b2538417ab
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-09-27 10:38:23 +0800

    Update:五险一金新增回退功能，同时调整关闭的过滤条件

--- zszq_SQL/嘉扬/01 薪资福利/40 五险一金/eSP_pEMPInsuranceHousingFundBack.sql
+++ zszq_SQL/嘉扬/01 薪资福利/40 五险一金/eSP_pEMPInsuranceHousingFundBack.sql
@@ -4,32 +4,23 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-ALTER  Procedure [dbo].[eSP_pEMPInsuranceHousingFundClose]
--- skydatarefresh eSP_pEMPInsuranceHousingFundClose
+ALTER  Procedure [dbo].[eSP_pEMPInsuranceHousingFundBack]
+-- skydatarefresh eSP_pEMPInsuranceHousingFundBack
     @ID int,
-    @URID int,  
     @RetVal int=0 Output
 As
 /*
 -- Create By wuliang E004205
--- 五险一金分配月度关闭程序
+-- 五险一金分配月度退回程序
 -- @ID 为五险一金分配月度对应ID
 -- @URID 为workshop操作账号的ID，前台通过 {U_URID} 全局参数获取
 */
 Begin
     
-    -- 月度五险一金分配未开启，无法关闭!
-    If Exists(Select 1 From pEMPInsuranceHousingFund_Process Where ID=@ID And Isnull(Submit,0)=0)
+    -- 月度五险一金分配未关闭，无法退回!
+    If Exists(Select 1 From pEMPInsuranceHousingFund_Process Where ID=@ID And Isnull(Closed,0)=0)
     Begin
-        Set @RetVal = 930440
-        Return @RetVal
-    End
-
-
-    -- 月度五险一金分配已关闭，无法关闭!
-    If Exists(Select 1 From pEMPInsuranceHousingFund_Process Where ID=@ID And Isnull(Closed,0)=1)
-    Begin
-        Set @RetVal = 930450
+        Set @RetVal = 930460
         Return @RetVal
     End
 
@@ -39,33 +30,33 @@ Begin
 
     -- 更新部门年金月度表项pEMPInsuranceHousingFundDep
     update a
-    set a.IsClosed=1
+    set a.IsClosed=NULL
     from pEMPInsuranceHousingFundDep a,pEMPInsuranceHousingFund_Process b
-    where b.ID=@ID and b.Date=a.Month and ISNULL(a.IsClosed,0)=0
+    where b.ID=@ID and b.Date=a.Month
     -- 异常流程
     If @@Error<>0
     Goto ErrM
 
-    -- 插入后台员工社保月度分配历史表pEMPInsurancePerMM_all
-    insert into pEMPInsurancePerMM_all(Month,EID,EndowInsEMP,EndowInsEMPPlus,MedicalInsEMP,MedicalInsEMPPlus,UnemployInsEMP,UnemployInsEMPPlus,
+    -- 插入后台员工社保月度分配历史表pEMPInsurancePerMM
+    insert into pEMPInsurancePerMM(Month,EID,EndowInsEMP,EndowInsEMPPlus,MedicalInsEMP,MedicalInsEMPPlus,UnemployInsEMP,UnemployInsEMPPlus,
     EndowInsGRP,EndowInsGRPPlus,MedicalInsGRP,MedicalInsGRPPlus,UnemployInsGRP,UnemployInsGRPPlus,MaternityInsGRP,MaternityInsGRPPlus,InjuryInsGRP,InjuryInsGRPPlus,
     InsEMPTotal,InsEMPPlusTotal,InsGRPTotal,InsGRPPlusTotal,Remark)
     select a.Month,a.EID,a.EndowInsEMP,a.EndowInsEMPPlus,a.MedicalInsEMP,a.MedicalInsEMPPlus,a.UnemployInsEMP,a.UnemployInsEMPPlus,a.EndowInsGRP,a.EndowInsGRPPlus,
     a.MedicalInsGRP,a.MedicalInsGRPPlus,a.UnemployInsGRP,a.UnemployInsGRPPlus,a.MaternityInsGRP,a.MaternityInsGRPPlus,a.InjuryInsGRP,a.InjuryInsGRPPlus,
     a.InsEMPTotal,a.InsEMPPlusTotal,a.InsGRPTotal,a.InsGRPPlusTotal,a.Remark
-    from pEMPInsurancePerMM a
-    where a.Month=(select Date from pEMPInsuranceHousingFund_Process where ID=@ID)
+    from pEMPInsurancePerMM_all a
+    where Month=(select Date from pEMPInsuranceHousingFund_Process where ID=@ID)
     -- 异常流程
     If @@Error<>0
     Goto ErrM
 
-    -- 插入后台员工公积金月度分配历史表pEMPHousingFundPerMM_all
-    insert into pEMPHousingFundPerMM_all(Month,EID,HousingFundEMP,HousingFundEMPPlus,HousingFundGRP,HousingFundGRPPlus,
+    -- 插入后台员工公积金月度分配历史表pEMPHousingFundPerMM
+    insert into pEMPHousingFundPerMM(Month,EID,HousingFundEMP,HousingFundEMPPlus,HousingFundGRP,HousingFundGRPPlus,
     HousingFundEMPTotal,HousingFundEMPPlusTotal,HousingFundGRPTotal,HousingFundGRPPlusTotal,Remark)
     select a.Month,a.EID,a.HousingFundEMP,a.HousingFundEMPPlus,a.HousingFundGRP,a.HousingFundGRPPlus,
     a.HousingFundEMPTotal,a.HousingFundEMPPlusTotal,a.HousingFundGRPTotal,a.HousingFundGRPPlusTotal,a.Remark
-    from pEMPHousingFundPerMM a
-    where a.Month=(select Date from pEMPInsuranceHousingFund_Process where ID=@ID)
+    from pEMPHousingFundPerMM_all a
+    where Month=(select Date from pEMPInsuranceHousingFund_Process where ID=@ID)
     -- 异常流程
     If @@Error<>0
     Goto ErrM
@@ -73,7 +64,7 @@ Begin
     
     -- 更新五险一金月度表项pEMPInsuranceHousingFund_Process
     Update a
-    Set a.Closed=1,a.ClosedBy=@URID,a.ClosedTime=GETDATE()
+    Set a.Closed=NULL,a.ClosedBy=NULL,a.ClosedTime=NULL
     From pEMPInsuranceHousingFund_Process a
     Where a.ID=@ID
     -- 异常流程
@@ -81,14 +72,14 @@ Begin
     Goto ErrM
 
     -- 删除员工社保月度注册表
-    delete from pEMPInsurancePerMM
+    delete from pEMPInsurancePerMM_all
     where Month=(select Date from pEMPInsuranceHousingFund_Process where ID=@ID)
     -- 异常流程
     If @@Error<>0
     Goto ErrM
 
     -- 删除员工公积金月度注册表
-    delete from pEMPHousingFundPerMM
+    delete from pEMPHousingFundPerMM_all
     where Month=(select Date from pEMPInsuranceHousingFund_Process where ID=@ID)
     -- 异常流程
     If @@Error<>0

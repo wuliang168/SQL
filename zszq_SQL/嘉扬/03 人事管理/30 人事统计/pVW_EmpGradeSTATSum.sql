58822fac917b1e66e05bbc0449ae08ab913d1d97
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-10-09 17:39:09 +0800

    Update:根据人事等级和部门进行人事统计

---
+++ zszq_SQL/嘉扬/03 人事管理/30 人事统计/pVW_EmpGradeSTATSum.sql
@@ -0,0 +1,7 @@
+-- pVW_EmpGradeSTATSum
+
+select a.DepType,a.EmpGrade,SUM(a.EMP) as EMPSum,SUM(a.GenderM) as GenderMSum,SUM(a.GenderF) as GenderFSum,SUM(a.EMP*a.AVGAGE)/SUM(a.EMP) as AVGAGESum,
+SUM(a.HighLev_ASSTT) as HighLev_ASSTTSum,SUM(a.HighLev_UGTT) as HighLev_UGTTSum,SUM(a.HighLev_MSTT) as HighLev_MSTTSum,SUM(a.HighLev_DTTT) as HighLev_DTTTSum,
+SUM(a.WorkDate1t5YTT) as WorkDate1t5YTTSum,SUM(a.WorkDate5t10YTT) as WorkDate5t10YTTSum,SUM(a.WorkDate10YTT) as WorkDate10YTTSum,SUM(a.JoinDate3YTT) as JoinDate3YTTSum
+from pVW_EmpGradeSTAT a
+group by a.DepType,a.EmpGrade

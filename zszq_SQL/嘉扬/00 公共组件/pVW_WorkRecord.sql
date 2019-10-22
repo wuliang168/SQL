a75cd109bc7d412913b61c048a61674a69aa0184
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-10-22 15:46:11 +0800

    Update:更新工作日志视图

--- zszq_SQL/嘉扬/00 公共组件/pVW_WorkRecord.sql
+++ zszq_SQL/嘉扬/00 公共组件/pVW_WorkRecord.sql
@@ -4,7 +4,7 @@ select a.EID as EID,a.WorkDay as WorkDay,a.month as mm,a.tianxieday as tianxieda
 from (select a.EID as EID,(Case When DATEDIFF(mm,b.joindate,GETDATE())<=1 Then  (select COUNT(term) from lCalendar where xType=1 and Datediff(mm,term,GETDATE())=1 and DATEDIFF(DD,b.JoinDate,term)>=0)
 ELSE (select COUNT(term) from lCalendar where xType=1 and Datediff(mm,term,GETDATE())=1) End) as WorkDay,
 DATEADD(m,-1 ,dateadd(dd,-day(getdate())+1,getdate())) as month,
-(select COUNT(EID) from Workrecord where EID=a.EID and DATEDIFF(mm,tianxiedate,GETDATE())=1 and id in (select min(id) from Workrecord where DATEDIFF(mm,tianxiedate,GETDATE())=1 group by EID,tianxiedate)) as tianxieday, a.Name as Name,a.DepID as depid1,
+(select COUNT(EID) from Workrecord where EID=a.EID and DATEDIFF(mm,tianxiedate,GETDATE())=1 and id in (select min(id) from Workrecord where DATEDIFF(mm,tianxiedate,GETDATE())=1 group by EID,CONVERT(VARCHAR(10),tianxiedate,110))) as tianxieday, a.Name as Name,a.DepID as depid1,
 (select DepAbbr from oDepartment where DepID=a.DepID) as DepAbbr,(select DepType from oDepartment where DepID=a.DepID) as DepType,(select xorder from oJob where JobID=a.JobID) as xOrder
 from eEmployee a
 inner join eStatus b on a.EID=b.EID

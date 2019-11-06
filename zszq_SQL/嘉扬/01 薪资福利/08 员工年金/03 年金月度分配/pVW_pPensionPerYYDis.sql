56a2b6b2031fb289bc25aa565bea66ca1ab8ce49
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-11-06 12:58:29 +0800

    Update:Fixbug修复关闭功能中更新剩余金额的视图

--- zszq_SQL/嘉扬/01 薪资福利/08 员工年金/03 年金月度分配/pVW_pPensionPerYYDis.sql
+++ zszq_SQL/嘉扬/01 薪资福利/08 员工年金/03 年金月度分配/pVW_pPensionPerYYDis.sql
@@ -1,15 +1,7 @@
 -- pVW_pPensionPerYYDis
 
-select a.PensionYear,a.EID,a.BID,ISNULL(a.EmpPensionPerYYRST,0) as EmpPensionPerYYRST_old,
-c.EmpPensionPerYYDisAccu as EmpPensionPerYYDisAccu,
-(case when c.EmpPensionPerYYDisAccu-b.EmpPensionPerMMTT>=0 then c.EmpPensionPerYYDisAccu-b.EmpPensionPerMMTT
-else 0 end) as EmpPensionPerYYRST_new,b.EmpPensionPerMMTT as EmpPensionPerMM
-from pPensionUpdatePerEmp a
-inner join (select EID,BID,(ISNULL(EmpPensionPerMMATax,0)+ISNULL(EmpPensionPerMMBTax,0)) as EmpPensionPerMMTT from pEmpPensionPerMM_register) as b on ISNULL(b.EID,b.BID)=ISNULL(a.EID,a.BID)
-left join (select m.EID,m.BID,m.PensionYear,
-(select SUM(ISNULL(EmpPensionPerYYRST,0)) from pPensionUpdatePerEmp where DATEDIFF(YY,PensionYear,m.PensionYear)>=0 and ISNULL(EID,BID)=ISNULL(m.EID,m.BID)) as EmpPensionPerYYDisAccu
-from pPensionUpdatePerEmp m
-where m.PensionYear in (select PensionYear from pPensionPerYY where ISNULL(IsDisMM,0)=1)
-group by m.EID,m.BID,m.PensionYear,m.EmpPensionPerYYRST) as c on ISNULL(a.EID,a.BID)=ISNULL(c.EID,c.BID) and DATEDIFF(YY,c.PensionYear,a.PensionYear)=0
-where YEAR(a.PensionYear) in (select YEAR(PensionYear) from pPensionPerYY where ISNULL(IsDisMM,0)=1)
-group by a.EID,a.BID,a.PensionYear,b.EmpPensionPerMMTT,c.EmpPensionPerYYDisAccu,a.EmpPensionPerYYRST
+select a.PensionYear,a.EID,a.BID,a.EmpPensionPerYYRST_old,a.EmpPensionPerYYDisAccu,a.EmpPensionPerYYDisAccu_diff,
+a.EmpPensionPerYYDisAccu_diff
+-ISNULL((select EmpPensionPerYYDisAccu_diff from pVW_pPensionPerYYDisDiff where DATEDIFF(YY,PensionYear,a.PensionYear)=1 and EID=a.EID),0) as EmpPensionPerYYRST_new,
+EmpPensionPerMM
+from pVW_pPensionPerYYDisDiff a

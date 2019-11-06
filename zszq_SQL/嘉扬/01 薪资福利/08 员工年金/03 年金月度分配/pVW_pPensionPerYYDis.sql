6d2da930e7fe42793aed412f6e57e22ab738652f
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-11-06 14:42:40 +0800

    Update:Fixbug修复前台未计算的问题

--- zszq_SQL/嘉扬/01 薪资福利/08 员工年金/03 年金月度分配/pVW_pPensionPerYYDis.sql
+++ zszq_SQL/嘉扬/01 薪资福利/08 员工年金/03 年金月度分配/pVW_pPensionPerYYDis.sql
@@ -2,6 +2,6 @@
 
 select a.PensionYear,a.EID,a.BID,a.EmpPensionPerYYRST_old,a.EmpPensionPerYYDisAccu,a.EmpPensionPerYYDisAccu_diff,
 a.EmpPensionPerYYDisAccu_diff
--ISNULL((select EmpPensionPerYYDisAccu_diff from pVW_pPensionPerYYDisDiff where DATEDIFF(YY,PensionYear,a.PensionYear)=1 and EID=a.EID),0) as EmpPensionPerYYRST_new,
+-ISNULL((select EmpPensionPerYYDisAccu_diff from pVW_pPensionPerYYDisDiff where DATEDIFF(YY,PensionYear,a.PensionYear)=1 and ISNULL(EID,BID)=ISNULL(a.EID,a.BID)),0) as EmpPensionPerYYRST_new,
 EmpPensionPerMM
 from pVW_pPensionPerYYDisDiff a

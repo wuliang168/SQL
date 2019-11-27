a55e01ec760b597ab2197f9520d57f66b1d17816
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-11-27 12:58:52 +0800

    Update:根据信息技术部需求，新增毕业院校和籍贯

--- zszq_SQL/嘉扬/04 其他部门/40 信息技术部/pVW_EmpInfoEN.sql
+++ zszq_SQL/嘉扬/04 其他部门/40 信息技术部/pVW_EmpInfoEN.sql
@@ -6,10 +6,10 @@ a.Badge as Badge, a.Name as Name,(select CompAbbr from oCompany where CompID=a.C
 (select DepType from oDepartment where DepID=a.DepID) as DepType,
 (select JobAbbr from oJob where JobID=a.JobID) as JobAbbr, (select Title from eCD_EmpGrade where ID=a.EmpGrade) as EmpGrade, (select Title from OCD_MDTYPE where ID=f.MDID) as MDID,
 (select Title from ECD_EMPSTATUS where ID=a.Status) as Status, (select Title from eCD_Marriage where ID=c.Marriage) as Marriage,(select Title from eCD_Party where ID=c.party) as Party,
-c.Address as Address,(select Title from eCD_City where ID=a.WorkCity) as WorkCity, 
+c.Address as Address,(select Title from eCD_Place where ID=c.Place) as Place,(select Title from eCD_City where ID=a.WorkCity) as WorkCity, 
 (select Title from eCD_Country where ID=c.Country) as Country, (select Title from eCD_CertType where ID=c.CertType) as CertType, c.CertNo as CertNo, 
 (select Title from eCD_Gender where ID=c.Gender) as Gender, c.BirthDay as BirthDay, DATEDIFF(yy, c.BirthDay, GETDATE()) AS Age, 
-(select Title from eCD_EduType where ID=c.HighLevel) as HighLevel, (select Title from eCD_DegreeType where ID=c.HighDegree) as HighDegree, 
+(select Title from eCD_EduType where ID=c.HighLevel) as HighLevel, (select Title from eCD_DegreeType where ID=c.HighDegree) as HighDegree,c.schoolname as SchoolName, 
 b.JoinDate as JoinDate, ROUND(DATEDIFF(mm, b.JoinDate, GETDATE()) / 12.00 + ISNULL(b.Cyear_adjust, 0), 2) AS Cyear, 
 b.JobBegindate as JobBegindate, ROUND(DATEDIFF(mm, b.JobBegindate, GETDATE())/12.00,2) AS Jyear, 
 c.WorkBeginDate as WorkBeginDate,ROUND(ISNULL(c.workyear_adjust, 0) + ROUND(DATEDIFF(mm, c.WorkBeginDate, GETDATE()) / 12.00, 2), 2) AS Wyear, 
@@ -21,6 +21,6 @@ INNER JOIN dbo.eStatus AS b ON a.EID = b.EID
 INNER JOIN dbo.eDetails AS c ON a.EID = c.EID 
 INNER JOIN dbo.oDepartment AS d ON a.DepID = d.DepID 
 INNER JOIN dbo.oJob AS e ON a.JobID = e.JobID
-INNER JOIN dbo.pEmployeeEmolu AS f ON a.EID=f.EID
+INNER JOIN dbo.pEMPAdminIDMD AS f ON a.EID=f.EID
 INNER JOIN dbo.SkySecUser AS g on a.EID=g.EID
 where a.Status not in (5)

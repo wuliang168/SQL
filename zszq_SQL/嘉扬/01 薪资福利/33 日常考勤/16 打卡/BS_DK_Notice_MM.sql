96ab5ba1a9af818f10297879c5122be1564dcbad
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-12-02 15:26:35 +0800

    Add:新增异常考勤月度自动提醒功能

---
+++ zszq_SQL/嘉扬/01 薪资福利/33 日常考勤/16 打卡/BS_DK_Notice_MM.sql
@@ -0,0 +1,35 @@
+USE [zszq]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER proc [dbo].[BS_DK_Notice_MM]
+as
+begin
+
+    -- 个人考勤异常提醒
+    ---- 短信内容提醒
+    INSERT OPENQUERY(ORCL, 'SELECT MOBILE, CONTENT FROM HRIS.QUEUES') 
+    select distinct C.Mobile,D.SMS
+    from ATTENCE_LLMONTHLY A,eEmployee B,eDetails C,pSMSInfo D
+    where (A.ABNORMWORKDAYS<>0 or A.LATEWORKDAYS<>0 or A.EARLYWORKDAYS<>0)
+    and dbo.eFN_getdeptype(ISNULL(A.Dep2nd,A.Dep1st))=1 and A.Dep1st<>349
+    and A.EID=B.EID and B.WorkCity=45 and A.EID=C.EID and D.ID=10
+    ---- 邮件内容提醒
+    INSERT INTO skyJobQueueSMTP(MailTo,Subject,Message,Status,starttime,endtime,Times,Timesmax) 
+    select distinct C.OA_mail,D.Subject,D.Message,0,GETDATE(),DATEADD(DD,1,GETDATE()),0,3
+    from ATTENCE_LLMONTHLY A,eEmployee B,eDetails C,pEMAILInfo D
+    where (A.ABNORMWORKDAYS<>0 or A.LATEWORKDAYS<>0 or A.EARLYWORKDAYS<>0)
+    and dbo.eFN_getdeptype(ISNULL(A.Dep2nd,A.Dep1st))=1 and A.Dep1st<>349
+    and A.EID=B.EID and B.WorkCity=45 and A.EID=C.EID and D.ID=1
+
+    -- 部门负责人短信提醒
+    ---- 短信内容提醒
+    INSERT OPENQUERY(ORCL, 'SELECT MOBILE, CONTENT FROM HRIS.QUEUES') 
+    select distinct C.Mobile,D.SMS
+    from ATTENCE_LLMMDEP A,eEmployee B,eDetails C,pSMSInfo D
+    where A.ReportToDaily=B.EID and B.DepID<>349
+    and A.ReportToDaily=C.EID and D.ID=13
+
+end

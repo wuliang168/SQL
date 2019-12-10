a57939e8de623458deac199663c7b29846936852
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2019-12-10 15:43:41 +0800

    Update:更新考勤提醒功能，减少金融衍生品部门

--- zszq_SQL/嘉扬/01 薪资福利/33 日常考勤/16 打卡/BS_DK_Notice_MM.sql
+++ zszq_SQL/嘉扬/01 薪资福利/33 日常考勤/16 打卡/BS_DK_Notice_MM.sql
@@ -14,14 +14,14 @@ begin
     select distinct C.Mobile,D.SMS
     from ATTENCE_LLMONTHLY A,eEmployee B,eDetails C,pSMSInfo D
     where (A.ABNORMWORKDAYS<>0 or A.LATEWORKDAYS<>0 or A.EARLYWORKDAYS<>0)
-    and dbo.eFN_getdeptype(ISNULL(A.Dep2nd,A.Dep1st))=1 and A.Dep1st<>349
+    and dbo.eFN_getdeptype(ISNULL(A.Dep2nd,A.Dep1st))=1 and A.Dep1st not in (349,394)
     and A.EID=B.EID and B.WorkCity=45 and A.EID=C.EID and D.ID=10
     ---- 邮件内容提醒
     INSERT INTO skyJobQueueSMTP(MailTo,Subject,Message,Status,starttime,endtime,Times,Timesmax) 
     select distinct C.OA_mail,D.Subject,D.Message,0,GETDATE(),DATEADD(DD,1,GETDATE()),0,3
     from ATTENCE_LLMONTHLY A,eEmployee B,eDetails C,pEMAILInfo D
     where (A.ABNORMWORKDAYS<>0 or A.LATEWORKDAYS<>0 or A.EARLYWORKDAYS<>0)
-    and dbo.eFN_getdeptype(ISNULL(A.Dep2nd,A.Dep1st))=1 and A.Dep1st<>349
+    and dbo.eFN_getdeptype(ISNULL(A.Dep2nd,A.Dep1st))=1 and A.Dep1st not in (349,394)
     and A.EID=B.EID and B.WorkCity=45 and A.EID=C.EID and D.ID=1
 
     -- 部门负责人短信提醒
@@ -29,7 +29,7 @@ begin
     INSERT OPENQUERY(ORCL, 'SELECT MOBILE, CONTENT FROM HRIS.QUEUES') 
     select distinct C.Mobile,D.SMS
     from ATTENCE_LLMMDEP A,eEmployee B,eDetails C,pSMSInfo D
-    where A.ReportToDaily=B.EID and B.DepID<>349
+    where A.ReportToDaily=B.EID and B.DepID not in (349,394)
     and A.ReportToDaily=C.EID and D.ID=13
 
 end

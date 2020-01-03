d0ee83c1a17973560223d26883b3b3b6a04232c5
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2020-01-03 15:18:58 +0800

    Update:调整考核结果状态从9到99

--- zszq_SQL/嘉扬/01 薪资福利/01 年度绩效考核/pVW_pYear_ScoreResult_All.sql
+++ zszq_SQL/嘉扬/01 薪资福利/01 年度绩效考核/pVW_pYear_ScoreResult_All.sql
@@ -1,11 +1,20 @@
 -- pVW_pYear_ScoreResult_All
 
+USE [zszq]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER VIEW [dbo].[pVW_pYear_ScoreResult_All]
+AS
+
 ---- pYear_Score
 select distinct EID,pYear_ID,Score_Status,Score_Type1,Score_Type2,Score_DepID,
 ScoreEach,ScoreSTG1,ScoreSTG2,ScoreSTG3,ScoreSTG4,ScoreSTG5,ScoreCompl,ScoreParty,Weight1,Weight2,Weight3,Modulus,
 TotalNum,TotalRankNum,ScoreYear,isRanking,Ranking,RankLevel,Remark
 from pYear_Score
-where Score_Status=9
+where Score_Status=99
 
 ---- pYear_Score_all
 UNION
@@ -13,4 +22,6 @@ select distinct EID,pYear_ID,Score_Status,Score_Type1,Score_Type2,Score_DepID,
 ScoreEach,ScoreSTG1,ScoreSTG2,ScoreSTG3,ScoreSTG4,ScoreSTG5,ScoreCompl,ScoreParty,Weight1,Weight2,Weight3,Modulus,
 TotalNum,TotalRankNum,ScoreYear,isRanking,Ranking,RankLevel,Remark
 from pYear_Score_all
-where Score_Status=9
+where Score_Status=99
+
+Go

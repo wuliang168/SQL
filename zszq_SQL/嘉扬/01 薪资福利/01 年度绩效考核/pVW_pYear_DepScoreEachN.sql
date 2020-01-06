206d41ed05d3556939c4e23859e0d1a9e7168d0b
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2020-01-06 13:05:49 +0800

    Update:根据最新考核类型编号修改

--- zszq_SQL/嘉扬/01 薪资福利/01 年度绩效考核/pVW_pYear_DepScoreEachN.sql
+++ zszq_SQL/嘉扬/01 薪资福利/01 年度绩效考核/pVW_pYear_DepScoreEachN.sql
@@ -1,5 +1,17 @@
 -- pVW_pYear_DepScoreEachN
+
+USE [zszq]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER VIEW [dbo].[pVW_pYear_DepScoreEachN]
+AS
+
 select pYear_ID as pYear_ID,Score_DepID as Score_DepID,COUNT(Score_DepID) as TotalNum,COUNT(Submit) as TotalScoreEachNNum
 from pYear_Score
-where Score_Status=1 and Score_Type1 in (4,11,29,12,13)
+where Score_Status=1 and Score_Type1 in (4,11,33)
 group by pYear_ID,Score_DepID
+
+GO

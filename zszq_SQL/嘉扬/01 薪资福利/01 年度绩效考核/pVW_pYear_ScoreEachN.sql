baa4f054a78e7dfcebe3eaf00cc4f2a60c5d66e5
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2020-01-07 12:22:49 +0800

    Update:总部员工互评，添加总部副职

--- zszq_SQL/嘉扬/01 薪资福利/01 年度绩效考核/pVW_pYear_ScoreEachN.sql
+++ zszq_SQL/嘉扬/01 薪资福利/01 年度绩效考核/pVW_pYear_ScoreEachN.sql
@@ -17,16 +17,16 @@ AS
 
 -- 4-总部普通员工
 ---- 不包含合规部门
-select a.EID as EID,a.Score_Type1 as Score_Type1,b.EID as Score_EID
+select a.EID as EID,a.Score_Type1 as Score_Type1,b.EID as Score_EID,b.Score_Type1 as ScoreEID_Type1
 from pEmployee_register a,pEmployee_register b,eEmployee c,eEmployee d
-where a.kpidepidyy=b.kpidepidyy and a.Score_Type1=4 and b.Score_Type1=4 and a.EID<>b.EID
+where a.kpidepidyy=b.kpidepidyy and a.Score_Type1=4 and b.Score_Type1 in (4,2) and a.EID<>b.EID
 and a.EID=c.EID and b.EID=d.EID and c.Status not in (4,5) and d.Status not in (4,5)
 and a.kpidepidyy<>737
 
 -- 11-子公司普通员工
 ---- 不包含合规部门
 UNION
-select a.EID as EID,a.Score_Type1 as Score_Type1,b.EID as Score_EID
+select a.EID as EID,a.Score_Type1 as Score_Type1,b.EID as Score_EID,b.Score_Type1 as ScoreEID_Type1
 from pEmployee_register a,pEmployee_register b,eEmployee c,eEmployee d
 where a.kpidepidyy=b.kpidepidyy and a.Score_Type1=11 and b.Score_Type1=11 and a.EID<>b.EID
 and a.EID=c.EID and b.EID=d.EID and c.Status not in (4,5) and d.Status not in (4,5)
@@ -34,7 +34,7 @@ and a.kpidepidyy not in (666,542)
 
 -- 33-分支机构普通员工
 UNION
-select a.EID as EID,a.Score_Type1 as Score_Type1,b.EID as Score_EID
+select a.EID as EID,a.Score_Type1 as Score_Type1,b.EID as Score_EID,b.Score_Type1 as ScoreEID_Type1
 from pEmployee_register a,pEmployee_register b,eEmployee c,eEmployee d
 where a.kpidepidyy=b.kpidepidyy and a.Score_Type1=33 and b.Score_Type1=33 and a.EID<>b.EID
 and a.EID=c.EID and b.EID=d.EID and c.Status not in (4,5) and d.Status not in (4,5)

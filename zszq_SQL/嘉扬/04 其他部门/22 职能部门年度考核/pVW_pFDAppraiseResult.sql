3606c3cab92d2b0d85f706d8272ab15b161eca7a
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2020-01-14 18:12:51 +0800

    Update:新增法律合规部的考核

--- zszq_SQL/嘉扬/04 其他部门/22 职能部门年度考核/pVW_pFDAppraiseResult.sql
+++ zszq_SQL/嘉扬/04 其他部门/22 职能部门年度考核/pVW_pFDAppraiseResult.sql
@@ -12,21 +12,27 @@ AS
 ---- 服务支持 10%
 select a.pYear_ID,a.DepID,a.Director,AVG(ScoreTotal) as ScoreTotal,0.1 as Modulus,NULL as FDAppraiseEID,N'服务支持' as FDAppraiseTitle,1 as xOrder
 from pFDAppraise a
-where a.Status in (1,2) and a.FDAppraiseType in (3,15,16)
+where a.Status in (1,2) and a.FDAppraiseType in (3,15,16) and a.DepID<>737
 group by a.pYear_ID,a.DepID,a.Director
 
 ---- 班子其他成员 20%
 UNION
 select a.pYear_ID,a.DepID,a.Director,SUM(a.ScoreTotal)/COUNT(a.ScoreTotal)*5 as ScoreTotal,0.2 as Modulus,NULL as FDAppraiseEID,N'班子其他成员' as FDAppraiseTitle,2 as xOrder
 from pFDAppraise a
-where a.Status in (6) and a.FDAppraiseType in (8,9,10,17,18)
+where a.Status in (6) and a.FDAppraiseType in (8,9,10,17,18) and a.DepID<>737
 group by a.pYear_ID,a.DepID,a.Director
 
 ---- 分管领导 20%
 UNION
 select a.pYear_ID,a.DepID,a.Director,SUM(ScoreTotal) as ScoreTotal,0.2 as Modulus,a.FDAppraiseEID as FDAppraiseEID,N'分管领导' as FDAppraiseTitle,2 as xOrder
 from pFDAppraise a
-where a.Status in (4) and a.FDAppraiseType in (8,9,10,17,18)
+where a.Status in (4) and a.FDAppraiseType in (8,9,10,17,18) and a.DepID<>737
+group by a.pYear_ID,a.DepID,a.Director,a.FDAppraiseEID
+------ 合规总监 100%
+UNION
+select a.pYear_ID,a.DepID,a.Director,SUM(ScoreTotal) as ScoreTotal,1 as Modulus,a.FDAppraiseEID as FDAppraiseEID,N'合规总监' as FDAppraiseTitle,2 as xOrder
+from pFDAppraise a
+where a.Status in (4) and a.FDAppraiseType in (8,9,10,17,18) and a.DepID=737
 group by a.pYear_ID,a.DepID,a.Director,a.FDAppraiseEID
 
 ---- 主要领导
@@ -34,21 +40,21 @@ group by a.pYear_ID,a.DepID,a.Director,a.FDAppraiseEID
 UNION
 select a.pYear_ID,a.DepID,a.Director,SUM(ScoreTotal) as ScoreTotal,0.25 as Modulus,a.FDAppraiseEID as FDAppraiseEID,N'董事长' as FDAppraiseTitle,5 as xOrder
 from pFDAppraise a
-where a.Status in (5) and a.FDAppraiseType in (8,9,10,17,18) and a.FDAppraiseEID=1022
+where a.Status in (5) and a.FDAppraiseType in (8,9,10,17,18) and a.FDAppraiseEID=1022 and a.DepID<>737
 group by a.pYear_ID,a.DepID,a.Director,a.FDAppraiseEID
 
 ------ 书记 12.5%
 UNION
 select a.pYear_ID,a.DepID,a.Director,SUM(ScoreTotal) as ScoreTotal,0.125 as Modulus,a.FDAppraiseEID as FDAppraiseEID,N'党委书记' as FDAppraiseTitle,4 as xOrder
 from pFDAppraise a
-where a.Status in (5) and a.FDAppraiseType in (8,9,10,17,18) and a.FDAppraiseEID=5587
+where a.Status in (5) and a.FDAppraiseType in (8,9,10,17,18) and a.FDAppraiseEID=5587 and a.DepID<>737
 group by a.pYear_ID,a.DepID,a.Director,a.FDAppraiseEID
 
 ------ 总裁 12.5%
 UNION
 select a.pYear_ID,a.DepID,a.Director,SUM(ScoreTotal) as ScoreTotal,0.125 as Modulus,a.FDAppraiseEID as FDAppraiseEID,N'总裁' as FDAppraiseTitle,3 as xOrder
 from pFDAppraise a
-where a.Status in (5) and a.FDAppraiseType in (8,9,10,17,18) and a.FDAppraiseEID=5014
+where a.Status in (5) and a.FDAppraiseType in (8,9,10,17,18) and a.FDAppraiseEID=5014 and a.DepID<>737
 group by a.pYear_ID,a.DepID,a.Director,a.FDAppraiseEID
 
 Go

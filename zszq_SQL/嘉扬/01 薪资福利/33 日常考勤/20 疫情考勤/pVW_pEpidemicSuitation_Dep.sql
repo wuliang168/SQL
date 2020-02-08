b9254e5bdec209c664d2c936a923b67337e8385f
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2020-02-08 23:20:32 +0800

    Update:调整疫情统计部门表项字段名称

--- zszq_SQL/嘉扬/01 薪资福利/33 日常考勤/20 疫情考勤/pVW_pEpidemicSuitation_Dep.sql
+++ zszq_SQL/嘉扬/01 薪资福利/33 日常考勤/20 疫情考勤/pVW_pEpidemicSuitation_Dep.sql
@@ -13,10 +13,10 @@ AS
 SELECT DISTINCT
 GETDATE() as ESDATE,b.approverID AS ApproverID, a.CompID as CompID,a.depid AS DepID,b.DepID1st AS DepID1st,b.DepID2nd AS DepID2nd,
 (select Name from eEmployee where EID=b.approverID) as approver,a.xOrder,
-(case when (select COUNT(Name) from pEpidemicSuitation where ReportTo=b.approverID and DATEDIFF(DD,GETDATE(),ESDATE)=0)>0 and b.SubmitStatus=1 then 1 when b.SubmitStatus=3 then 2 else 0 end) as SubmitStatus,
+(case when (select COUNT(Name) from pEpidemicSuitation where ReportTo=b.approverID and DATEDIFF(DD,GETDATE(),ESDATE)=0)>0 and b.SubmitType=1 then 1 when b.SubmitType=3 then 2 else 0 end) as SubmitStatus,
 (select COUNT(ReportTo) from pEpidemicSuitation where ReportTo=b.approverID and DATEDIFF(DD,GETDATE(),ESDATE)=0) as DepCount
 FROM oDepartment a,pEpidemicSuitation_Dep b
 WHERE a.DepID=b.DepID
-and b.SubmitStatus in (1,3)
+and b.SubmitType in (1,3)
 
 Go

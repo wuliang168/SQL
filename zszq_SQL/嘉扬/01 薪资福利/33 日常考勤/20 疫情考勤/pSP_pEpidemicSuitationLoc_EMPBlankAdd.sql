0d08c3c52ba0a5e19dea92aae95d4ef72ba0efc2
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2020-02-14 17:15:31 +0800

    Add:新增6类人员的新增功能

---
+++ zszq_SQL/嘉扬/01 薪资福利/33 日常考勤/20 疫情考勤/pSP_pEpidemicSuitationLoc_EMPBlankAdd.sql
@@ -0,0 +1,42 @@
+USE [zszq]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER proc [dbo].[pSP_pEpidemicSuitationLoc_EMPBlankAdd]
+    @ReportTo int,
+    @RetVal int=0 OutPut
+as
+/*
+-- Create By wuliang E004205
+-- 疫情现场办公新增空白外包员工
+*/
+Begin
+
+
+    Begin TRANSACTION
+
+    -------- pEpidemicSuitationLoc --------
+    -- 新增员工
+    insert into pEpidemicSuitationLoc(Type,CompID,DepID,DepID1st,DepID2nd,ReportTo)
+    select 1,a.CompID,a.DepID,a.DepID1st,a.DepID2nd,@ReportTo
+    from pVW_employee a
+    where a.EID=@ReportTo
+    -- 异常处理
+    IF @@Error <> 0
+    Goto ErrM
+
+
+    -- 正常处理流程
+    COMMIT TRANSACTION
+    Set @RetVal=0
+    Return @RetVal
+
+    -- 异常处理流程
+    ErrM:
+    ROLLBACK TRANSACTION
+    If isnull(@RetVal,0)=0
+        Set @RetVal=-1
+    Return @RetVal
+end

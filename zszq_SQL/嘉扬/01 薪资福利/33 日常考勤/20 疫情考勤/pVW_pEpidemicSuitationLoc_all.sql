8b560b25d0b227e1795cdc3348a47a5c2b5aafe6
wuliang168 <35696405+wuliang168@users.noreply.github.com>
2020-02-28 14:12:51 +0800

    Update:新增人员类别

--- zszq_SQL/嘉扬/01 薪资福利/33 日常考勤/20 疫情考勤/pVW_pEpidemicSuitationLoc_all.sql
+++ zszq_SQL/嘉扬/01 薪资福利/33 日常考勤/20 疫情考勤/pVW_pEpidemicSuitationLoc_all.sql
@@ -13,7 +13,8 @@ select a.EID,a.BID,a.Name,a.ESTDATE,a.CompID as CompID,a.DepID as DepID,a.DepID1
 (case when ISNULL(a.BID,a.EID) is not NULL then b.DepType else dbo.eFN_getdeptype(a.DepID) end) as DepType,
 a.ReportTo,a.EpidemicType,a.IsReturn,a.IsDS,a.IsDSC,a.GLBDate,a.GLEDate,a.Position,a.ESTReturnDate,a.ESTWorkDate,
 a.Submit,a.SubmitBy,a.SubmitTime,
-(case when ISNULL(a.BID,a.EID) is not NULL then b.JobxOrder else 999999999999999 end) as jobxorder
+(case when ISNULL(a.BID,a.EID) is not NULL then b.JobxOrder else 999999999999999 end) as jobxorder,
+(case when ISNULL(a.BID,a.EID) is not NULL then 1 else 0 end) as EMPType
 from pEpidemicSuitationLoc_all a
 left join pVW_employee b on ISNULL(a.BID,a.EID)=ISNULL(b.BID,b.EID)
 where DATEDIFF(dd,a.ESTDATE,GETDATE())=0
